1a2128ab89126f02a3f0cbf4102518614873aeb4
John Pretz <pretz@icecube.umd.edu>
2004-01-28 22:32:12 +0000

    added some templates
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@579 16731396-06f5-0310-8873-f7f720988828

--- project.mk
+++ project.mk
@@ -1,6 +1,10 @@
 ROOT_BIN_NAMES := 
 ROOT_EXCLUDE_NAMES := 
 ROOT_NON_CLASSES :=
-ROOT_TEMPLATES := 
+ROOT_TEMPLATES := I3Iter \
+I3Iterator \
+I3SingleItemIterator \
+I3ZeroItemIterator \
+I3TCollectionIterator 
 USES_PROJECTS := icetray 
 USES_TOOLS := root
