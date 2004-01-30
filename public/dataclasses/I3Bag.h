cbbc90ae4d5d1fbb03cceef190ebbe89aa5ebbeb
John Pretz <pretz@icecube.umd.edu>
2004-01-30 16:30:11 +0000

    Added a bag.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@596 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3Bag.h
@@ -0,0 +1,27 @@
+#ifndef I3BAG_H
+#define I3BAG_h
+
+/**
+ * Class: I3Bag
+ *
+ * Version: $Id: I3Bag.h,v 1.1 2004/01/30 16:30:11 pretz Exp $
+ *
+ * Date: 30 Jan 2004
+ *
+ * (c) 2004 IceCube collaboration
+ *
+ */
+
+#include "TObject.h"
+
+/**
+ * I3Bag is a generic keyed TObject container.  It isn't fully implemented
+ * now.  Each event gets one so that  
+ *
+ *
+ */
+class I3Bag : public TObject{
+
+};
+
+#endif //I3BAG_H
