eade6c299dc7853586bb0baf3f50de2becf89ee8
John Pretz <pretz@icecube.umd.edu>
2004-02-02 15:37:28 +0000

    Added the required 'explict template instantiation' for the classes that are arguments of the Iterator templates.  Needed so that the classes are actually made into shared libraries for ROOT
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@608 16731396-06f5-0310-8873-f7f720988828

---
+++ private/explicit-templates/I3Iter-constI3RecoHit.cxx
@@ -0,0 +1,30 @@
+/*
+ * class: None - just template declarations
+ *
+ * Version $Id: I3Iter-constI3RecoHit.cxx,v 1.1 2004/02/02 15:37:28 pretz Exp $
+ *
+ * Date: 29 Sept 2003
+ *
+ * (c) IceCube Collaboration
+ */
+
+// Header files
+#include "dataclasses/I3Iterator.h"
+#include "dataclasses/I3SingleItemIterator.h"
+#include "dataclasses/I3ZeroItemIterator.h"
+#include "dataclasses/I3TCollectionIterator.h"
+#include "dataclasses/I3Iter.h"
+#include "dataclasses/I3RecoHit.h"
+
+ClassImpT(I3Iterator,Type);
+ClassImpT(I3SingleItemIterator,Type);
+ClassImpT(I3ZeroItemIterator,Type);
+ClassImpT(I3TCollectionIterator,Type);
+ClassImpT(I3Iter,Type);
+
+template I3Iterator<const I3RecoHit>;
+template I3SingleItemIterator<const I3RecoHit>;
+template I3ZeroItemIterator<const I3RecoHit>;
+template I3TCollectionIterator<const I3RecoHit>;
+template I3Iter<const I3RecoHit>;
+
