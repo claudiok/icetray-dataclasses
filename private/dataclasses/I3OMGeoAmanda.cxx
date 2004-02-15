5701565b8aae460d21daafbf9bd3f8f33626a8a4
Troy Straszheim <troy@icecube.umd.edu>
2004-02-15 19:49:46 +0000

    another one bites the dust!
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@646 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3OMGeoAmanda.cxx
+++ private/dataclasses/I3OMGeoAmanda.cxx
@@ -1,6 +1,18 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3OMGeoAmanda.cxx,v 1.2 2004/02/15 19:49:46 troy Exp $
+
+    @version $Revision: 1.2 $
+    @date $Date: 2004/02/15 19:49:46 $
+    @author
+
+    @todo
+
+*/
 #include "dataclasses/I3OMGeoAmanda.h"
 
 ClassImp(I3OMGeoAmanda);
 
-I3OMGeoAmanda::I3OMGeoAmanda()  {;}
+
 
