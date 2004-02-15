8350fadbd0a0694c355e330154424b67a4e2ac5b
Troy Straszheim <troy@icecube.umd.edu>
2004-02-15 19:12:35 +0000

    coding conventions
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@640 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3Digital.cxx
+++ private/dataclasses/I3Digital.cxx
@@ -1,10 +1,16 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Digital.cxx,v 1.2 2004/02/15 19:12:35 troy Exp $
+
+    @version $Revision: 1.2 $
+    @date $Date: 2004/02/15 19:12:35 $
+    @author
+
+    @todo
+
+*/
 #include "dataclasses/I3Digital.h"
 
 ClassImp(I3Digital);
   
-I3Digital::I3Digital() {value=0;}
-
-unsigned char I3Digital::GetValue() const                {return(value);}
-void          I3Digital::SetValue(unsigned char value_)  {value=value_;}
-
-
--- public/dataclasses/I3Digital.h
+++ public/dataclasses/I3Digital.h
@@ -1,3 +1,15 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Digital.h,v 1.2 2004/02/15 19:12:14 troy Exp $
+
+    @version $Revision: 1.2 $
+    @date $Date: 2004/02/15 19:12:14 $
+    @author
+
+    @todo
+
+*/
 #ifndef I3DIGITAL_H
 #define I3DIGITAL_H
 
@@ -5,13 +17,13 @@
 
 class I3Digital : public TObject
 {
-  unsigned char value;
+  UChar_t fValue;
 
   public:
-  I3Digital();
+  I3Digital() { fValue = 0; }
 
-  unsigned char GetValue() const;
-  void SetValue(unsigned char value_);
+  UChar_t Value() const { return fValue; }
+  void Value(UChar_t value_) { fValue = value_; }
 
   ClassDef(I3Digital, 1);
 };
