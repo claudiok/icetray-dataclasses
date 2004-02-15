fb0757537ddfec6a7a7022775bd1907d62a7a015
Troy Straszheim <troy@icecube.umd.edu>
2004-02-15 14:23:33 +0000

    LinkDef file with explicit typedefs for root
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@632 16731396-06f5-0310-8873-f7f720988828

---
+++ LinkDef.h
@@ -0,0 +1,6 @@
+#pragma link C++ typedef I3RecoHitSeries;
+#pragma link C++ typedef I3RecoHitSeriesStoragePolicy;
+
+#pragma link C++ typedef I3MCHitSeries;
+#pragma link C++ typedef I3MCHitSeriesStoragePolicy;
+
