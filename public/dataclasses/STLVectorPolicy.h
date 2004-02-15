8a4a718c770f12ad8ca4441e4cb7a5c2a4791be9
Troy Straszheim <troy@icecube.umd.edu>
2004-02-15 02:33:33 +0000

    stl vector storage policy class
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@628 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/STLVectorPolicy.h
@@ -0,0 +1,48 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: STLVectorPolicy.h,v 1.1 2004/02/15 02:33:33 troy Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2004/02/15 02:33:33 $
+    @author Troy Straszheim
+
+*/
+#ifndef STLVECTOR_POLICY_INCLUDED
+#define STLVECTOR_POLICY_INCLUDED
+
+#include <vector>
+using namespace std;
+
+template <class ElementType>
+class STLVectorPolicy {
+
+ public:
+
+  typedef vector<ElementType>                           vector_type;
+  typedef typename vector_type::iterator                iterator;
+  typedef typename vector_type::const_iterator          const_iterator;
+  typedef typename vector_type::reverse_iterator        reverse_iterator;
+  typedef ElementType& 					reference;
+  typedef const ElementType& 				const_reference;
+
+  void push_back(const ElementType &e) {
+    vector_.push_back(e);
+  }
+
+  reference operator[](size_t n) { return vector_[n]; }
+
+  const_reference operator[](size_t n) const { return vector_[n]; }
+
+  size_t size() const { return vector_.size(); }
+  void resize(size_t newsize_) { vector_.resize(newsize_); }
+  iterator begin() { return vector_.begin(); }
+  iterator end() { return vector_.end(); }
+  
+ protected:
+
+  vector_type vector_;
+
+};
+
+#endif
