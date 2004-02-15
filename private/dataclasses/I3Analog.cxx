61f98e2da6a758de8e76fb7e789089114622d121
Troy Straszheim <troy@icecube.umd.edu>
2004-02-15 18:21:29 +0000

    coding conventions
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@639 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3Analog.cxx
+++ private/dataclasses/I3Analog.cxx
@@ -1,25 +1,26 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Analog.cxx,v 1.2 2004/02/15 18:20:37 troy Exp $
+
+    @version $Revision: 1.2 $
+    @date $Date: 2004/02/15 18:20:37 $
+    @author
+
+    @todo
+
+*/
 #include "dataclasses/I3Analog.h"
 
 ClassImp(I3Analog);
   
-I3Analog::I3Analog()
+void
+I3Analog::Init()
 {
-  time=0;
-  let=0;
-  integratedcharge=0;
-  tot=0;
-  peak=0;
+  fTime=0;
+  fLet=0;
+  fIntegratedcharge=0;
+  fTot=0;
+  fPeak=0;
 }
-
-unsigned long  I3Analog::GetTime() const             {return(time);}
-unsigned long  I3Analog::GetLet() const              {return(let);}
-float          I3Analog::GetIntegratedCharge() const {return(integratedcharge);}
-unsigned short I3Analog::GetTot() const              {return(tot);}
-float          I3Analog::GetPeak() const             {return(peak);}
-
-void I3Analog::SetTime(unsigned long time_)                 {time=time_;}
-void I3Analog::SetLet(unsigned long let_)                   {let=let_;}
-void I3Analog::SetIntegratedCharge(float integratedcharge_) {integratedcharge=integratedcharge_;}
-void I3Analog::SetTot(unsigned short tot_)                  {tot=tot_;}
-void I3Analog::SetPeak(float peak_)                         {peak=peak_;}
 
--- public/dataclasses/I3Analog.h
+++ public/dataclasses/I3Analog.h
@@ -1,3 +1,15 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Analog.h,v 1.2 2004/02/15 18:21:29 troy Exp $
+
+    @version $Revision: 1.2 $
+    @date $Date: 2004/02/15 18:21:29 $
+    @author
+
+    @todo
+
+*/
 #ifndef I3ANALOG_H
 #define I3ANALOG_H
 
@@ -5,26 +17,31 @@
 
 class I3Analog : public TObject
 {
-  unsigned long  time;
-  unsigned long  let;
-  float          integratedcharge;
-  unsigned short tot;
-  float          peak;
+  ULong_t  fTime;
+  ULong_t  fLet;
+  Float_t          fIntegratedcharge;
+  UShort_t fTot;
+  Float_t          fPeak;
 
   public:
-  I3Analog();
+  void Init();
 
-  unsigned long  GetTime() const;
-  unsigned long  GetLet() const;
-  float          GetIntegratedCharge() const;
-  unsigned short GetTot() const;
-  float          GetPeak() const;
+  I3Analog() { Init(); }
 
-  void SetTime(unsigned long time_);
-  void SetLet(unsigned long let_);
-  void SetIntegratedCharge(float integratedcharge_);
-  void SetTot(unsigned short tot_);
-  void SetPeak(float peak_);
+  ULong_t  Time() const { return fTime; }
+  void  Time(ULong_t t) { fTime = t; }
+
+  ULong_t  Let() const { return fLet; }
+  void  Let(ULong_t l)  { fLet = l; }
+
+  Float_t IntegratedCharge() const { return fIntegratedcharge; }
+  void IntegratedCharge(Float_t i) { fIntegratedcharge = i; }
+
+  UShort_t Tot() const { return fTot; }
+  void Tot(UShort_t t) { fTot = t; }
+  
+  Float_t Peak() { return fPeak; }
+  void Peak(Float_t peak_) { fPeak = peak_; }
 
   ClassDef(I3Analog, 1);
 };
