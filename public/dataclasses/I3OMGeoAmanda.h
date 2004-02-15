84e347b04211f0834e5f3906c4a5e1dd2edca3a2
Troy Straszheim <troy@icecube.umd.edu>
2004-02-15 19:50:51 +0000

    <Bass> DAAAA Dum, Dum, Dum.  Da dumdum dada dum!  Dum dum dum.... </Bass>
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@647 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3OMGeoAmanda.h
+++ public/dataclasses/I3OMGeoAmanda.h
@@ -1,3 +1,15 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3OMGeoAmanda.h,v 1.2 2004/02/15 19:50:51 troy Exp $
+
+    @version $Revision: 1.2 $
+    @date $Date: 2004/02/15 19:50:51 $
+    @author
+
+    @todo
+
+*/
 #ifndef I3OMGEOAMANDA_H
 #define I3OMGEOAMANDA_H
 
@@ -7,7 +19,7 @@ class I3OMGeoAmanda : public I3OMGeo
 {
 
   public:
-  I3OMGeoAmanda();
+  I3OMGeoAmanda() {;}
 
   ClassDef(I3OMGeoAmanda,1);
 };
