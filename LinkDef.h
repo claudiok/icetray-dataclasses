a005b921a45453056ccb2faa1a5711c58915c78a
Troy Straszheim <troy@icecube.umd.edu>
2004-02-16 01:33:14 +0000

    cant forget this
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@653 16731396-06f5-0310-8873-f7f720988828

--- LinkDef.h
+++ LinkDef.h
@@ -4,3 +4,19 @@
 #pragma link C++ typedef I3MCHitSeries;
 #pragma link C++ typedef I3MCHitSeriesStoragePolicy;
 
+#pragma link C++ typedef I3AnalogSeries;
+#pragma link C++ typedef I3AnalogSeriesStoragePolicy;
+
+#pragma link C++ typedef I3DigitalSeries;
+#pragma link C++ typedef I3DigitalSeriesStoragePolicy;
+
+#pragma link C++ typedef I3DigitalATWDSeries;
+#pragma link C++ typedef I3DigitalFADCSeries;
+#pragma link C++ typedef I3DigitalTWRSeries;
+
+#pragma link C++ typedef I3DataReadoutList;
+#pragma link C++ typedef I3DataReadoutListStoragePolicy;
+
+#pragma link C++ typedef I3RecoTrackList;
+#pragma link C++ typedef I3RecoTrackListStoragePolicy;
+
