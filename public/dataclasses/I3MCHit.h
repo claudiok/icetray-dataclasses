564e99b70d6ed6304988f48981b9af306e5a4f91
Troy Straszheim <troy@icecube.umd.edu>
2004-02-17 22:55:38 +0000

    copy constructor and assignment operator
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@668 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3MCHit.h
+++ public/dataclasses/I3MCHit.h
@@ -1,10 +1,10 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: I3MCHit.h,v 1.2 2004/02/15 02:21:35 troy Exp $
+    $Id: I3MCHit.h,v 1.3 2004/02/17 22:55:38 troy Exp $
 
-    @version $Revision: 1.2 $
-    @date $Date: 2004/02/15 02:21:35 $
+    @version $Revision: 1.3 $
+    @date $Date: 2004/02/17 22:55:38 $
     @author
 
 */
@@ -21,6 +21,13 @@ class I3MCHit : public I3Hit
 
   public:
   I3MCHit() { fWeight = 0; fTrackNum=0;}
+  I3MCHit(const I3MCHit &rhs) { *this = rhs; }
+
+  const I3MCHit& operator=(const I3MCHit &rhs) {
+    fWeight = rhs.fWeight;
+    fTrackNum = rhs.fTrackNum; 
+    return *this;
+  }
 
   Float_t Weight() const { return fWeight; }
   void Weight(Float_t weight_) { fWeight = weight_; }
