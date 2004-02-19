4e082633eca5b8cc0bee0badc0892f6c57bc2f68
Peter Niessen <niessen@ifh.de>
2004-02-19 17:57:45 +0000

    Added a header and forgotten const qualifiers.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@683 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3Geometry.h
+++ public/dataclasses/I3Geometry.h
@@ -1,3 +1,18 @@
+/**
+
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Geometry.h,v 1.8 2004/02/19 17:57:45 niessen Exp $
+
+    @version $Revision: 1.8 $
+    @date $Date: 2004/02/19 17:57:45 $
+    @author Some lazy sucker who did not put the header here.
+
+    @todo 
+
+*/
+
+
 #ifndef I3GEOMETRY_H
 #define I3GEOMETRY_H
 
@@ -10,10 +25,10 @@ class I3GeometryHeader {
   Double_t     fTime;
   Long_t       fDate;
  public:
-  Double_t Time() { return fTime; }
+  Double_t Time() const { return fTime; }
   void Time(Double_t time_) { fTime = time_; }
 
-  Long_t Date() {  return fDate; }
+  Long_t Date() const {  return fDate; }
   void Date(Long_t date_) { fDate = date_; }
 };
 
