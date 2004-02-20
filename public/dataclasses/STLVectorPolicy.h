4f360b7abecd4157cf170c57f43dab34b721c13f
Troy Straszheim <troy@icecube.umd.edu>
2004-02-20 04:05:09 +0000

    type_iterator added
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@688 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/STLVectorPolicy.h
+++ public/dataclasses/STLVectorPolicy.h
@@ -1,10 +1,10 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: STLVectorPolicy.h,v 1.5 2004/02/18 21:39:46 troy Exp $
+    $Id: STLVectorPolicy.h,v 1.6 2004/02/20 04:05:09 troy Exp $
 
-    @version $Revision: 1.5 $
-    @date $Date: 2004/02/18 21:39:46 $
+    @version $Revision: 1.6 $
+    @date $Date: 2004/02/20 04:05:09 $
     @author Troy Straszheim
 
 */
@@ -26,6 +26,47 @@ class StoragePolicy {
   typedef ElementType& 					reference;
   typedef const ElementType& 				const_reference;
 
+  template <class Type> class type_iterator
+    {
+    public:
+
+      const vector_type* thevec;
+      typename vector_type::iterator iter;
+
+
+      type_iterator() { 
+	thevec=NULL; 
+      }
+      type_iterator(const type_iterator& rhs) { *this = rhs; }
+      ~type_iterator() {;}
+
+      type_iterator& operator=(const type_iterator& rhs) {
+	thevec = rhs.thevec;
+	iter = rhs.iter;
+	return *this;
+      }
+
+      // here we need type and bounds checking
+      type_iterator& operator++(int i) 
+	{
+	  iter++; 
+	  while (iter != thevec->end())
+	    {
+	      Type t = dynamic_cast<Type>(*iter);
+	      if (t!=NULL) { return *this; }
+	      iter++;
+	    }
+	  return *this;
+	}
+      
+      Type operator->() { return dynamic_cast<Type>(*iter); }
+      Type operator*() { return dynamic_cast<Type>(*iter); }
+      bool operator!=(const type_iterator<Type> &otheriter) {
+	return (iter != otheriter.iter);
+      }
+    };
+
+
  private:
   vector_type vector_;
 
@@ -43,9 +84,29 @@ class StoragePolicy {
   void reserve (size_t n) { vector_.reserve(n); }
 
 
+  // these need to be template member functions with defaults to
+  // ElementType (for a "regular" element)
   iterator begin() { return vector_.begin(); }
   iterator end() { return vector_.end(); }
   
+  template <class Type> type_iterator<Type> begin() 
+    { 
+      type_iterator<Type> titer;
+      titer.thevec = &vector_;
+      titer.iter = vector_.begin();
+      if(dynamic_cast<Type>(*(titer.iter)) == NULL)
+	titer++;
+      return titer;
+    }
+
+  template <class Type> type_iterator<Type> end() 
+    { 
+      type_iterator<Type> titer;
+      titer.iter = vector_.end();
+      titer.thevec = &vector_;
+      return titer;
+    }
+
   reference front() { return vector_.front(); }
   const_reference front() const { return vector_.front(); }
 
@@ -55,6 +116,9 @@ class StoragePolicy {
   //rootcint meckert about friend function somethingorother
   //  bool empty() const { return vector_.empty() }
 
+  // dont do this it is messy
+  // virtual ~StoragePolicy() {;}
+  // ClassDef(StoragePolicy,1);
 };
 
 #endif
