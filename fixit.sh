90b15c1251ad67b097cef2b8261221866be90fcf
John Pretz <pretz@icecube.umd.edu>
2004-02-21 14:54:58 +0000

    This script fixes the 'libraries not loading' problem.  run it from the workspace.  I've filed an issue on this with 'issuezilla' and I guess they'll fix it someday.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@700 16731396-06f5-0310-8873-f7f720988828

---
+++ fixit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# this script fixes the problem that log4cpp isn't loaded when an
+# executable is run.  run this script from you workspace
+# -J.Pretz
+cd tools/Linux-i386/lib/ ; ln -sf liblog4cpp.so liblog4cpp.so.3
