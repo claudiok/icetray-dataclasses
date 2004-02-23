fc845a5a95f4e85b8fc54fd87902259f8f78bc55
John Pretz <pretz@icecube.umd.edu>
2004-02-23 17:14:06 +0000

    Moved it over to the standard Map
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@714 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3Bag.cxx
+++ private/dataclasses/I3Bag.cxx
@@ -10,59 +10,10 @@ using namespace std;
 
 ClassImp(I3Bag);
 
-I3Bag::I3Bag() :
-  fMap(0)
+I3Bag::I3Bag()
 {
 }
 
 I3Bag::~I3Bag()
 {
-  if(fMap){
-    fMap->DeleteAll();
-    delete fMap;
-  }
-}
-
-Bool_t I3Bag::AddObject(TObject* obj,const Char_t* key)
-{
-  if(0==fMap)
-    fMap = new TMap();
-
-  if(fMap->FindObject(key))
-    return kFALSE;
-  
-  fMap->Add(new TNamed(key,""),obj);
-  return kTRUE;
-}
-
-const TObject& I3Bag::GetObject(const Char_t* key, const Char_t* type)
-{
-  if(!HasObject(key,type)){
-    I3DataExecution::Instance().Fatal("I3Bag::GetObject() the named object isn't present");
-    return *(TObject*)0;
-  }
-  TPair* pair = (TPair*)fMap->FindObject(key);
-  return *(pair->Value());
-}
-
-Bool_t I3Bag::HasObject(const Char_t* key,const Char_t* type)
-{
-  if(!fMap)
-    {
-      cout<<"the map doesn't exist"<<endl;
-      return kFALSE;
-    }
-  TObject* to_check = fMap->FindObject(key);
-  if(!to_check)
-    {
-      cout<<"Didn't succeed in getting the object"<<endl;
-      return kFALSE; 
-    }
-  TPair *pair = (TPair*)to_check;
-  if(!pair->Value()->InheritsFrom(type))
-    {
-      cout<<"The inheritance isn't right"<<endl;
-      return kFALSE;
-    }
-  return kTRUE;
 }
