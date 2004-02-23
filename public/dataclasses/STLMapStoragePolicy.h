1b9ac4f8fe89aa0e51deffd58d710d3611f8da9e
Troy Straszheim <troy@icecube.umd.edu>
2004-02-23 21:45:51 +0000

    more iterators and operations on STLMapStoragePolicy
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@723 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/STLMapStoragePolicy.h
+++ public/dataclasses/STLMapStoragePolicy.h
@@ -1,10 +1,10 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: STLMapStoragePolicy.h,v 1.3 2004/02/23 17:31:21 troy Exp $
+    $Id: STLMapStoragePolicy.h,v 1.4 2004/02/23 21:45:51 troy Exp $
 
-    @version $Revision: 1.3 $
-    @date $Date: 2004/02/23 17:31:21 $
+    @version $Revision: 1.4 $
+    @date $Date: 2004/02/23 21:45:51 $
     @author Troy Straszheim
 
 */
@@ -24,20 +24,37 @@ class STLMapStoragePolicy {
   typedef string KeyType;
   typedef map<KeyType,ElementType> map_type;
   typedef typename map_type::iterator iterator;
+  typedef typename map_type::const_iterator const_iterator;
+  typedef typename map_type::size_type size_type;
 
  private:
 
   map_type map_;
 
+  STLMapStoragePolicy& operator=(const STLMapStoragePolicy &rhs) {
+    if (this == &rhs) return *this;
+    map_ = rhs.map_;
+    return *this;
+  }
+
  public:
+
+  // these operations all mirror standard STL map operations
 
   ElementType& operator[](const KeyType& key) { return map_[key]; }
   //  const ElementType& operator[](const KeyType& key) const { return map_[key]; }
-  iterator find (const KeyType& key) { return map_.find(key); }
   iterator begin() { return map_.begin(); }
+  const_iterator begin() const { return map_.begin(); }
   iterator end() { return map_.end(); }
+  const_iterator end() const { return map_.end(); }
 
+  bool empty() const { return map_.empty(); }
+  void clear() { map_.clear(); }
+  size_type size() const { return map_.size(); }
+  size_type max_size() const { return map_.max_size(); }
 
+  size_type erase(const KeyType &key) { return map_.erase(key); }
+  iterator find (const KeyType& key) { return map_.find(key); }
 };
 
 #endif
