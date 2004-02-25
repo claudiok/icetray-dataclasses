730a59346de39ce6bb6ea6bf4afb49b645ea2ffc
John Pretz <pretz@icecube.umd.edu>
2004-02-25 00:32:37 +0000

    some comments added
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@732 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3DataReadoutList.h
+++ public/dataclasses/I3DataReadoutList.h
@@ -1,15 +1,18 @@
 /**
-    copyright  (C) 2004
-    the icecube collaboration
-    $Id: I3DataReadoutList.h,v 1.10 2004/02/24 14:41:30 pretz Exp $
-
-    @version $Revision: 1.10 $
-    @date $Date: 2004/02/24 14:41:30 $
-    @author
-
-    @todo
-
-*/
+ * copyright  (C) 2004
+ * the icecube collaboration
+ * $Id: I3DataReadoutList.h,v 1.11 2004/02/25 00:32:37 pretz Exp $
+ *
+ * Just an list of I3DataReadouts.  
+ *
+ * @version $Revision: 1.11 $
+ * @date $Date: 2004/02/25 00:32:37 $
+ * @author ehrlich
+ * @author troy
+ * @author pretz
+ *
+ * @todo Shouldn't this be a list of DataReadoutPtrs ???
+ */
 #ifndef I3DATAREADOUTLIST_H
 #define I3DATAREADOUTLIST_H
 
@@ -19,15 +22,24 @@
 class I3DataReadoutList : public VectorPolicy<I3DataReadout>::ThePolicy
 {
 
-/*   I3DataReadoutList(const I3DataReadout&); */
-/*   const I3DataReadoutList& operator=(const I3DataReadout&); */
 
  public:
-
-
+  /**
+   * constructor
+   */ 
   I3DataReadoutList(){};
+
+  /**
+   * destructor 
+   */
   virtual ~I3DataReadoutList(){};
 
+ private:
+  // copy and assignment are private
+  I3DataReadoutList(const I3DataReadout&);
+  const I3DataReadoutList& operator=(const I3DataReadout&);
+
+  // ROOT macro
   ClassDef(I3DataReadoutList,1);
 };
 
