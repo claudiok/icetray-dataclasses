e63d324060201c1232812d81f2f26fc93996d261
John Pretz <pretz@icecube.umd.edu>
2004-03-09 14:00:19 +0000

    Added in the possibility to store the 'used tubes' and the 'parameters' as keyed doubles.  Now I think the full f2k data can be incorporated in a natural way.  There's a bug in root with regard to the map<string,double> Parameters() in the interpreter.  If that doesn't work itself out, may have to do this differently
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@762 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3RecoResultRDMCFit.h
+++ public/dataclasses/I3RecoResultRDMCFit.h
@@ -1,17 +1,17 @@
 /**
  * copyright  (C) 2004
  * the icecube collaboration
- * $Id: I3RecoResultRDMCFit.h,v 1.2 2004/03/09 12:21:54 pretz Exp $
+ * $Id: I3RecoResultRDMCFit.h,v 1.3 2004/03/09 14:00:19 pretz Exp $
  *
  * A container for RDMC fits.  Contains a vector of doubles which are
  * the fit parameters and a vector of ints which are the tubes used by
  * the fit.
  *
- * @version $Revision: 1.2 $
- * @date $Date: 2004/03/09 12:21:54 $
+ * @version $Revision: 1.3 $
+ * @date $Date: 2004/03/09 14:00:19 $
  * @author pretz
  *
- * @todo 
+ * @todo make sure the map<string,double> works in interpreted code
  *
  */
 
@@ -21,14 +21,16 @@
 #include "I3RecoResultSingleTrack.h"
 #include "dataclasses/I3DataExecution.h"
 #include <vector>
+#include <map>
+#include <string>
 
 using namespace std;
 
 class I3RecoResultRDMCFit : public I3RecoResultSingleTrack
 {
-  vector<Double_t> fParameters;
+  map<string,double> fParameters;
   vector<Int_t> fUsedTubes;
-
+  string fFitType;
   public:
   /**
    * constructor
@@ -41,16 +43,26 @@ class I3RecoResultRDMCFit : public I3RecoResultSingleTrack
   virtual ~I3RecoResultRDMCFit(){}
 
   /**
+   * @return the type of this fit as a const object
+   */
+  const string& FitType() const {return fFitType;}
+
+  /**
+   * @return the type of this fit as non-const object
+   */
+  string& FitName() {return fFitType;}
+
+  /**
    * The parameters of the RDMC fit in the order they appear in the f2k file
    * @return the parameters of this fit as a const object
    */
-  const vector<Double_t>& Parameters() const {return fParameters;}
+  const map<string,double>& Parameters() const {return fParameters;}
 
   /**
    * The parameters of the RDMC fit in the order they appear in the f2k file
    * @reeturn the parameters of this fit as a non-const object
    */
-  vector<Double_t>& Parameters() {return fParameters;}
+  map<string,double>& Parameters() {return fParameters;}
 
   /**
    * @return tubes used by this fit as a const object
@@ -68,7 +80,7 @@ class I3RecoResultRDMCFit : public I3RecoResultSingleTrack
   const I3RecoResultRDMCFit& operator=(const I3RecoResultRDMCFit&);
 
   // ROOT macro
-  ClassDef(I3RecoResultRDMCFit, 1);
+  ClassDef(I3RecoResultRDMCFit,1);
 };
 
 /**
