97e441447e2af3d258ddf5d0843823cacaecd32e
John Pretz <pretz@icecube.umd.edu>
2004-04-16 18:48:56 +0000

    Added this class
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/branches/NAN-tracks@821 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3NonDirectional.h
@@ -0,0 +1,20 @@
+#ifndef I3NONDIRECTIONAL_H
+#define I3NONDIRECTIONAL_H
+
+#include "Rtypes.h"
+
+class I3NonDirectional 
+{
+ public:
+  virtual bool IsNonDirectional() const {return true;}
+
+  virtual double Theta() const {return NAN;}
+  virtual void Theta(double theta) {return;}
+
+  virtual double Phi() const {return NAN;}
+  virtual void Phi(double phi) {return;}
+
+  ClassDef(I3NonDirectional,1);
+};
+
+#endif // I3NONDIRECTIONAL
