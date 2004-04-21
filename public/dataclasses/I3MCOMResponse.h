b90c3266e03cbcb8d65e368093d66f87418091d8
Spencer Klein <srklein@lbl.gov>
2004-04-21 18:48:25 +0000

    Split I3OMResponse into two files: I3OMResponse and I3MCOMResponse
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@841 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3MCOMResponse.h
@@ -0,0 +1,73 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3MCOMResponse.h,v 1.1 2004/04/21 18:48:25 spencer Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2004/04/21 18:48:25 $
+ * @author ehrlich
+ * @author troy
+ * @author pretz
+ * @author klein
+     
+ * @todo
+
+*/
+#ifndef I3MCOMRESPONSE_H
+#define I3MCOMRESPONSE_H
+
+#include "I3MCHitSeries.h"
+#include "I3MCPMTResponse.h"
+
+/**
+ * This contains all of the Monte Carlo OM Response information:
+ * the 'true' Monte Carlo hits and the simulated PMT response.
+ * all of the data and OM output is inherited from I3OMResponse
+ */
+class I3MCOMResponse : public I3OMResponse
+{
+  // with the following, you either put in the two pipes or get
+  // "cant instantiate precompiled template
+  // even though you can branch an I3MCHitSeries, or whatever.
+  // let us all hate root together for a moment
+  I3MCHitSeries       fMCHitSeries; //||
+  I3MCPMTResponse     fMCPMTResponse; //
+
+ public:
+  /**
+   * constructor
+   */
+  I3MCOMResponse()
+  
+  /**
+   * destructor
+   */
+    virtual ~I3MCOMResponse()
+  
+  /**
+   * @return the PMTResponse as a const object
+   */
+    const I3MCPMTResponse & const {return fMCPMTReponse;}
+  
+  /**
+   * @return the MCTruth for this response as a const object
+   */
+  const I3MCHitSeries& MCHitSeries() const {return fMCHitSeries;}
+
+  /**
+   * @return the MC truth for this response as a non-const object
+   */
+  I3MCHitSeries& MCHitSeries() {return fMCHitSeries;}
+
+  private:
+  // copy and assignment are private
+   I3MCOMResponse(const I3MCOMResponse&); 
+   const I3MCOMResponse& operator=(const I3MCOMResponse&); 
+
+  // ROOT macro
+  ClassDef(I3MCOMResponse,1);
+};
+
+typedef I3MCOMResponse* I3MCOMResponsePtr;
+#endif
+
