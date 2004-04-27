55249b37065557012d809e3732f167558f550daa
Tyce DeYoung <deyoung@phys.psu.edu>
2004-04-27 18:12:20 +0000

    Fixed DecayingMuon, had some leftover contamination from Lollipop.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@884 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3DecayingMuon.h
+++ public/dataclasses/I3DecayingMuon.h
@@ -1,39 +1,47 @@
 /**
     copyright  (C) 2004
     the IceCube collaboration
-    $Id: I3DecayingMuon.h,v 1.3 2004/04/27 02:32:05 pretz Exp $
+    $Id: I3DecayingMuon.h,v 1.4 2004/04/27 18:12:20 deyoung Exp $
 
-    @version $Revision: 1.3 $
-    @date $Date: 2004/04/27 02:32:05 $
+    @file I3DecayingMuon.h
+    @version $Revision: 1.4 $
+    @date $Date: 2004/04/27 18:12:20 $
     @author deyoung
-
-    @todo What's up with this class? -JP
-
-    For tau leptons which enter the detector and then decay (single
-    bang events).
-
 */
+
 
 #include "I3TrackImpl.h"
 #include "I3Stopping.h"
 #include "I3Composite.h"
 #include "I3Energetic.h"
 
-/* #ifndef I3LOLLIPOP_H */
-/* #define I3LOLLIPOP_H */
+#ifndef I3LOLLIPOP_H
+#define I3LOLLIPOP_H
 
-/* #include "dataclasses/I3Track.h" */
+#include "dataclasses/I3Track.h"
 
-/* class I3Lollipop : public I3TrackImpl<I3Identifiable, */
-/*                                           I3Stopping, */
-/*                                           I3Energetic, */
-/*                                           I3Composite> */
-/* { */
-/*  public: */
-/*   I3Lollipop(){}; */
-/*   virtual ~I3Lollipop(){} */
-/*   ClassDef(I3Lollipop,1); */
-/* }; */
+/**
+ * @brief For muons which decay in flight, with an EM shower at the
+ * decay vertex.
+ */
+class I3DecayingMuon : public I3TrackImpl<I3Stopping,
+                                      I3Energetic,
+                                      I3Composite>
+{
+ public:
+  /**
+   * constructor
+   */
+  I3DecayingMuon(){};
 
-/* #endif */
+  /**
+   * destructor
+   */
+  virtual ~I3DecayingMuon(){}
 
+ private:
+  //ROOT macro
+  ClassDef(I3DecayingMuon,1);
+};
+
+#endif
