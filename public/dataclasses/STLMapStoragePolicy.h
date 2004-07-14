5944fece4147b6e1e8a129d13adc396fc99a251c
John Pretz <pretz@icecube.umd.edu>
2004-07-14 16:01:01 +0000

    Extended the interface.  Basically just added some convention-following options to the stl thingies.  Also commented the methods following the gcc documentation.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@985 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/STLMapStoragePolicy.h
+++ public/dataclasses/STLMapStoragePolicy.h
@@ -1,11 +1,11 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: STLMapStoragePolicy.h,v 1.9 2004/07/12 16:12:46 pretz Exp $
+    $Id: STLMapStoragePolicy.h,v 1.10 2004/07/14 16:01:01 pretz Exp $
 
     @file STLMapStoragePolicy.h
-    @version $Revision: 1.9 $
-    @date $Date: 2004/07/12 16:12:46 $
+    @version $Revision: 1.10 $
+    @date $Date: 2004/07/14 16:01:01 $
     @author Troy Straszheim
 
 */
@@ -19,7 +19,13 @@ using namespace std;
 
 /**
  * @brief A template which provides a restricted interface to the
- * STL map class.
+ * STL map class.  
+ *
+ * The text for the documentation of stl methods is taken from the gnu 
+ * libstdc++3 documentation
+ * found here: http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/
+ *
+ * @todo fuller documentation needed
  */
 template <class KeyType, class ElementType>
 class STLMapStoragePolicy {
@@ -46,19 +52,146 @@ class STLMapStoragePolicy {
 
   // these operations all mirror standard STL map operations
 
+  /**
+   * Subscript ( [] ) access to map data.
+   * @param key The key for which data should be retrieved.
+   * @return A reference to the data of the (key,data) pair.
+   */ 
   ElementType& operator[](const KeyType& key) { return map_[key]; }
+
+  /**
+   * The same as operator[]
+   */
+  ElementType& Get(const KeyType& key) {return map_[key];}
+
+  /**
+   * puts some data in the map with a particular key.  
+   * @param element the data to be copied into the map
+   * @param key the key used to retrieve the data
+   * @return true if the key isn't already used and the put succeeds,
+   * false otherwise.
+   */
+  bool Put(const ElementType& element, const KeyType& key)
+    {
+      iterator iter = find(key);
+      if(iter == end())
+	return false;
+      else
+	this->operator[](key) = element;
+    }
+
   //  const ElementType& operator[](const KeyType& key) const { return map_[key]; }
+
+  /**
+   * the same as begin()
+   */
+  iterator Begin() { return map_.begin();}
+
+  /**
+   * Returns a read/write iterator that points to the first pair in the map. 
+   * Iteration is done in ascending order according to the keys.
+   */
   iterator begin() { return map_.begin(); }
+
+  /**
+   * the same as begin() const
+   */
+  const_iterator Begin() const { return map_.begin();}
+
+  /**
+   * Returns a read-only (constant) iterator that points to the first pair 
+   * in the map. Iteration is done in ascending order according to the keys
+   */
   const_iterator begin() const { return map_.begin(); }
+
+  /**
+   * the same as end
+   */
+  iterator End() {return map_.end();}
+
+  /**
+   * Returns a read/write iterator that points one past the last pair in the 
+   * map. Iteration is done in ascending order according to the keys.
+   */
   iterator end() { return map_.end(); }
+
+  /**
+   * the same as end() const
+   */
+  const_iterator End() const { return map_.end();}
+
+  /**
+   * Returns a read-only (constant) iterator that points one past the last 
+   * pair in the map. Iteration is done in ascending order according to the 
+   * keys.
+   */
   const_iterator end() const { return map_.end(); }
 
+  /**
+   * same as empty()
+   */
+  bool IsEmpty() const {return map_.empty();}
+
+  /**
+   * Returns true if the map is empty. (Thus begin() would equal end().)
+   */
   bool empty() const { return map_.empty(); }
+  
+  /**
+   * the same as clear()
+   */
+  void Clear() { map_.clear();}
+  
+  /**
+   * Erases all elements in a map. Note that this function only erases the 
+   * elements, and that if the elements themselves are pointers, the 
+   * pointed-to memory is not touched in any way. Managing the pointer is 
+   * the user's responsibilty.
+   */
   void clear() { map_.clear(); }
+
+  /**
+   * same as size()
+   */
+  size_type GetSize() const {return map_.size();}
+
+  /**
+   * Returns the size of the map.
+   */
   size_type size() const { return map_.size(); }
+
+  /**
+   * same as max_size()
+   */
+  size_type GetMaxSize() const {return map_.max_size();}
+
+  /**
+   * Returns the maximum size of the map.
+   */
   size_type max_size() const { return map_.max_size(); }
 
+  /**
+   * same as erase()
+   */
+  size_type Erase(const KeyType& key) { return map_.erase(key);}
+
+  /**
+   * Erases elements according to the provided key.
+   * @param key Key of element to be erased.
+   * @return The number of elements erased.
+   */
   size_type erase(const KeyType &key) { return map_.erase(key); }
+
+  /**
+   * same as find()
+   */
+
+  iterator Find(const KeyType& key) { return map_.find(key);}
+  /**
+   * Tries to locate an element in a map.
+   * @param key Key of (key, value) pair to be located.
+   * @return Iterator pointing to sought-after element, or end() if not found.
+   */
   iterator find (const KeyType& key) { return map_.find(key); }
 };
 
--- public/dataclasses/STLVectorStoragePolicy.h
+++ public/dataclasses/STLVectorStoragePolicy.h
@@ -1,11 +1,11 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: STLVectorStoragePolicy.h,v 1.5 2004/04/27 13:35:23 pretz Exp $
+    $Id: STLVectorStoragePolicy.h,v 1.6 2004/07/14 16:01:01 pretz Exp $
 
     @file STLVectorStoragePolicy.h
-    @version $Revision: 1.5 $
-    @date $Date: 2004/04/27 13:35:23 $
+    @version $Revision: 1.6 $
+    @date $Date: 2004/07/14 16:01:01 $
     @author Troy Straszheim
 */
 #ifndef STLVECTOR_POLICY_INCLUDED
@@ -17,6 +17,11 @@ using namespace std;
 /**
  * @brief A template container akin to a STL vector but with a slightly 
  * restricted interface
+ *
+ * Note that the documentation text here comes from the gnu libstdc++3
+ * documentaiton at http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/
+ *
+ * @todo better documentation of the methods needed.
  */
 template <class ElementType>
 class STLVectorStoragePolicy  {
@@ -77,23 +82,136 @@ class STLVectorStoragePolicy  {
 
  public:
 
+  /**
+   * the same as push_back()
+   */
+  void Add(const ElementType& e) { vector_.push_back(e);}
+
+  /**
+   * Add data to the end of the vector.
+   * @param e Data to be added.
+   */
   void push_back(const ElementType &e) { vector_.push_back(e); }
+
+  /**
+   * Removes last element.
+   */
   void pop_back() { vector_.pop_back(); }
+
+  /**
+   * the same as clear()
+   */
+  void Clear() { vector_.clear();}
+
+  /**
+   * Erases all the elements. Note that this function only erases the 
+   * elements, and that if the elements themselves are pointers, the 
+   * pointed-to memory is not touched in any way. Managing the pointer 
+   * is the user's responsibilty.
+   */
   void clear() { vector_.clear(); }
 
+  /**
+   * Subscript access to the data contained in the vector.
+   * @param n The index of the element for which data should be accessed.
+   * @return Read/write reference to data.
+   */
   reference operator[](size_t n) { return vector_[n]; }
+
+  /**
+   * Subscript access to the data contained in the vector.
+   * @param The index of the element for which data should be accessed.
+   * @return Read-only (constant) reference to data.
+   */
   const_reference operator[](size_t n) const { return vector_[n]; }
 
+  /**
+   * the same as operator[]()
+   */
+  reference Get(size_t n) {return vector_[n];}
+
+  /**
+   * the same as operator[]()
+   */
+  const_reference Get(size_t n) const {return vector_[n];}
+
+  /**
+   * the same as size()
+   */
+  size_t Size() const {return vector_.size();}
+
+  /**
+   * Returns the number of elements in the vector.
+   */
   size_t size() const { return vector_.size(); }
+
+  /**
+   * the same as resize()
+   */
+  void Resize(size_t newsize) { vector_.resize(newsize);}
+
+  /**
+   * Resizes the vector to the specified number of elements.
+   */
   void resize(size_t newsize_) { vector_.resize(newsize_); }
+
+  /**
+   * the same reserve()
+   */
+  void Reserve(size_t n) { vector_.reserve(n);}
+
+  /**
+   * Attempt to preallocate enough memory for specified number of elements.
+   * @param n Number of elements required.
+   */
   void reserve (size_t n) { vector_.reserve(n); }
 
 
   // these need to be template member functions with defaults to
   // ElementType (for a "regular" element)
+
+  /**
+   * same as begin()
+   */
+  iterator Begin() { return vector_.begin();}
+
+  /**
+   * Returns a read/write iterator that points to the first element 
+   * in the vector. Iteration is done in ordinary element order.
+   */
   iterator begin() { return vector_.begin(); }
+
+  /**
+   * same as begin() const
+   */
+  const_iterator Begin() const {return vector_.begin();}
+
+  /**
+   * Returns a read-only (constant) iterator that points to the first 
+   * element in the vector. Iteration is done in ordinary element order.
+   */
   const_iterator begin() const {return vector_.begin();}
+
+  /**
+   * same as end()
+   */
+  iterator End() { return vector_.end();}
+
+  /**
+   * Returns a read/write iterator that points one past the last element 
+   * in the vector. Iteration is done in ordinary element order.
+   */
   iterator end() { return vector_.end(); }
+
+  /**
+   * same as end() const
+   */
+  const_iterator End() const {return vector_.end();}
+
+  /**
+   * Returns a read-only (constant) iterator that points one past the 
+   * last element in the vector. Iteration is done in ordinary element order.
+   */
   const_iterator end() const {return vector_.end();}
   
   template <class Type> type_iterator<Type> begin() 
@@ -114,10 +232,48 @@ class STLVectorStoragePolicy  {
       return titer;
     }
 
+  /**
+   * same as front() 
+   */
+  reference Front() { return vector_.front();}
+
+  /**
+   * Returns a read/write reference to the data at the first element of 
+   * the vector.
+   */
   reference front() { return vector_.front(); }
+
+  /**
+   * same as front() const
+   */
+  const_reference Front() const { return vector_.front();}
+
+  /**
+   * Returns a read-only (constant) reference to the data at the first 
+   * element of the vector.
+   */
   const_reference front() const { return vector_.front(); }
 
+  /**
+   * same as back()
+   */
+  reference Back() { return vector_.back();}
+
+  /**
+   * Returns a read/write reference to the data at the last element of the 
+   * vector.
+   */
   reference back() { return vector_.back(); }
+
+  /**
+   * same as back() const
+   */
+  const_reference Back() const { return vector_.back();}
+
+  /**
+   * Returns a read-only (constant) reference to the data at the last 
+   * element of the vector.
+   */
   const_reference back() const { return vector_.back(); }
 
   //rootcint meckert about friend function somethingorother
