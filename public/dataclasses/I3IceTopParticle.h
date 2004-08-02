5baa04eb676babcfef9bdaf78efe125e9318cf2b
Erik Blaufuss <blaufuss@icecube.umd.edu>
2004-08-02 20:07:00 +0000

    Removed InIce stuff from IceTop class.  Do'h
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1037 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3IceTopParticle.h
+++ public/dataclasses/I3IceTopParticle.h
@@ -1,11 +1,11 @@
 /**
  * copyright  (C) 2004
  * the icecube collaboration
- * $Id: I3IceTopParticle.h,v 1.1 2004/08/02 19:59:58 blaufuss Exp $
+ * $Id: I3IceTopParticle.h,v 1.2 2004/08/02 20:07:00 blaufuss Exp $
  *
  * @file I3IceTopParticle.h
- * @version $Revision: 1.1 $
- * @date $Date: 2004/08/02 19:59:58 $
+ * @version $Revision: 1.2 $
+ * @date $Date: 2004/08/02 20:07:00 $
  * @author blaufuss
  */
 #ifndef I3ICETOPPARTICLE_H
@@ -36,27 +36,6 @@ class I3IceTopParticle : public I3Particle
   virtual Bool_t IsIceTop() const {return kTRUE;}
 
   
-  /**
-   * indicates that this particle is a Cascade, and so dynamic casting to
-   * I3Cascade should be ok
-   */
-  virtual Bool_t IsCascade() = 0;
-
-  /**
-   * indicates that this particle is a track and so dynamic casting to I3Track
-   * should be ok
-   */
-  virtual Bool_t IsTrack() = 0;
-
-  /**
-   * Gives an position along the track, at time T.
-   */
-  virtual I3Position GetPos() const = 0;
-
-  /**
-   * Gives a T along the track, corresponding to X,Y and Z
-   */
-  virtual Double_t GetT() const = 0;
  private:
   //ROOT macro
   ClassDef(I3IceTopParticle,1);
