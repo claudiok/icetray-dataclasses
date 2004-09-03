ab793a6d8a5f97bc87114561545b6edb83b5045e
Peter Niessen <niessen@ifh.de>
2004-09-03 23:20:09 +0000

    Test to reflect the new order in I3TopGeometry.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1126 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3TopGeometryTest.cxx
+++ private/test/I3TopGeometryTest.cxx
@@ -1,10 +1,10 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: I3TopGeometryTest.cxx,v 1.3 2004/08/09 12:13:10 troy Exp $
+    $Id: I3TopGeometryTest.cxx,v 1.4 2004/09/03 23:20:09 niessen Exp $
 
-    @version $Revision: 1.3 $
-    @date $Date: 2004/08/09 12:13:10 $
+    @version $Revision: 1.4 $
+    @date $Date: 2004/09/03 23:20:09 $
     @author pretz
 
     @todo
@@ -12,9 +12,14 @@
 
 #include "TUT/tut.h"
 
-#include "dataclasses/I3TopGeometry.h"
-#include "TFile.h"
 #include <string>
+
+#include <TFile.h>
+
+#include "dataclasses/I3TopGeometry.h"
+#include "dataclasses/I3OMGeoIceTop.h"
+#include "dataclasses/I3TankGeo.h"
+#include "dataclasses/I3SurfModuleGeo.h"
 
 
 namespace tut
@@ -39,11 +44,58 @@ namespace tut
   void object::test<1>() 
   {
     I3TopGeometry geo;
-    I3TopStationGeoPtr station(new I3TopStationGeo);
-    I3TankGeoPtr tank(new I3TankGeo);
-    tank->SetTankHeight(45.0);
-    station->push_back(tank);
-    geo[0] = station;
+
+    // create the setup, from inside to outside
+    I3OMGeoIceTopPtr om00 (new I3OMGeoIceTop);
+    I3OMGeoIceTopPtr om01 (new I3OMGeoIceTop);
+    I3OMGeoIceTopPtr om10 (new I3OMGeoIceTop);
+    I3OMGeoIceTopPtr om11 (new I3OMGeoIceTop);
+
+    I3OMGeoIceTop &om00_p = *om00; // create pointers for reference
+    I3OMGeoIceTop &om01_p = *om01;
+    I3OMGeoIceTop &om10_p = *om10;
+    I3OMGeoIceTop &om11_p = *om11;
+
+    I3Position p_om00 (I3Position (-.5, 0., 0.));
+    I3Position p_om01 (I3Position (+.5, 0., 0.));
+    I3Position p_om10 (I3Position (-.5, 0., 0.));
+    I3Position p_om11 (I3Position (+.5, 0., 0.));
+
+    om00->SetPos (p_om00);
+    om01->SetPos (p_om01);
+    om10->SetPos (p_om10);
+    om11->SetPos (p_om11);
+
+    I3TankGeoPtr tank0 (new I3TankGeo);
+    tank0->SetHeight (45.0);
+    //tank0->push_back (om00_p);
+    //tank0->push_back (om01_p);
+    I3TankGeoPtr tank1 (new I3TankGeo);
+    tank1->SetHeight (90.0);
+    //tank1->push_back (om10_p);
+    //tank1->push_back (om11_p);
+
+    I3TankGeo &tank0_p = *tank0;
+    I3TankGeo &tank1_p = *tank1;
+
+    I3StationGeoPtr station (new I3StationGeo);
+    //station->push_back (tank0_p); // point to the 
+    //station->push_back (tank1_p);
+
+    // create the lists/maps in the geometry
+
+    // the station
+    geo.GetStationMap ()[StationKey (0)] = station;
+
+    // the tanks
+    geo.GetSurfModuleVector ().push_back (tank0);
+    geo.GetSurfModuleVector ().push_back (tank1);
+
+    // the oms
+    geo.GetOMTopMap ()[OMKey (1, 61)] = om00;
+    geo.GetOMTopMap ()[OMKey (1, 62)] = om01;
+    geo.GetOMTopMap ()[OMKey (1, 63)] = om10;
+    geo.GetOMTopMap ()[OMKey (1, 64)] = om11;
 
     TFile file_out("test.out.root","RECREATE");
 
@@ -59,9 +111,17 @@ namespace tut
 
     ensure("pointer is non-zero",geo_in!=0);
     
-    ensure("size read-in is right",geo_in->size()==1);
-    ensure("size of station geo is right",(*geo_in)[0]->size()==1);
-    ensure("tank height is right",(*(*geo_in)[0])[0]->GetTankHeight() == 45.0);
+#if 0
+    I3TankGeo &tank_0
+      = dynamic_cast<I3TankGeo &>(geo_in->GetSurfModuleVector ()[0]);
+    I3TankGeo &tank_1
+      = dynamic_cast<I3TankGeo &>(geo_in->GetSurfModuleVector ()[1]);
+    ensure("tank 0 height is right", tank_0.GetHeight () == 45.0);
+    ensure("tank 1 height is right", tank_1.GetHeight () == 90.0);
+    ensure("size of surfdet list is right",geo_in
+	   ->GetSurfModuleVector ().size () == 2);
+#endif
+	   
   }
 
 }
