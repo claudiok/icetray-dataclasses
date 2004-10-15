0120288ec0d341b531fc0eb59e4fbfc9c7f427e8
Troy Straszheim <troy@icecube.umd.edu>
2004-10-15 10:04:22 +0000

    Shift a couple things for forward-compatibility with makev2.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1177 16731396-06f5-0310-8873-f7f720988828

--- project.mk
+++ project.mk
@@ -2,7 +2,13 @@
 # add CXX_BIN_NAMES := myfile
 
 ROOT_BIN_NAMES := 
-CXX_BIN_NAMES := dataclasses-test
+
+# This will create two binaries which do the same thing due to the way
+# things are linked in makev1 (and other projects like phys-services
+# will run the tests from this library... This is a bug.  Things are
+# set up this way for compatibility with makev2, which links things
+# differently, doesn't have the bug
+CXX_BIN_NAMES := dataclasses-test dataclasses-test-autogen
 ROOT_EXCLUDE_NAMES := 
 ROOT_NON_CLASSES := 
 ROOT_TEMPLATES := 
