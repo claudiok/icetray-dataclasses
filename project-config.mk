84b4b54d29295e68e3b8717105ab6fb500c481d6
Troy Straszheim <troy@icecube.umd.edu>
2004-10-15 17:47:04 +0000

    config for offline
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1178 16731396-06f5-0310-8873-f7f720988828

---
+++ project-config.mk
@@ -0,0 +1,15 @@
+
+# put a file in private containing main()
+# add CXX_BIN_NAMES := myfile
+
+CXX_BIN_SRCDIR_NAMES := 
+TEST_SRCDIR_NAMES := test test-autogen
+ROOT_BIN_NAMES := 
+
+ROOT_EXCLUDE_NAMES := 
+ROOT_NON_CLASSES := 
+ROOT_TEMPLATES := 
+USES_PROJECTS := icetray 
+USES_TOOLS := root log4cplus roost TUT
+
+BUILD_FLAVOR := root
