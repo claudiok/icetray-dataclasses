305c90aa811c2b5caa004f0b1ceb1b6789c2692f
no_author <no_author@no_author>
2005-01-25 23:33:44 +0000

    This commit was manufactured by cvs2svn to create branch 'serialized'.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/branches/serialized@1263 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3MCCorePrimary.h
@@ -0,0 +1,39 @@
+/**
+ * copyright  (C) 2004
+ * the icecube collaboration
+ * $Id: I3MCCorePrimary.h,v 1.1 2005/01/24 23:17:44 ehrlich Exp $
+ *
+ * @file I3MCCorePrimary.h
+ * @version $Revision: 1.1 $
+ * @date $Date: 2005/01/24 23:17:44 $
+ * @author ehrlich
+ * @author pretz
+ */
+#ifndef I3MCCOREPRIMARY_H
+#define I3MCCOREPRIMARY_H
+
+#include "dataclasses/StoragePolicy.h"
+#include "I3PrimaryImpl.h"
+
+#include "I3Directional.h"
+#include "I3CoreLocalized.h"
+#include "I3Energetic.h"
+
+/**
+ * @brief The basic class for a Monte Carlo primary, where all the data is 
+ * known
+ */
+class I3MCCorePrimary : public I3PrimaryImpl<I3Directional,
+	                                 I3CoreLocalized,
+	                                 I3Energetic> 
+{
+  ClassDef(I3MCCorePrimary,1);
+};
+
+/**
+ * Pointer typedeffed away to insulate users from the 
+ * memory-mananagement implementation
+ */
+typedef PtrPolicy<I3MCCorePrimary>::ThePolicy I3MCCorePrimaryPtr;
+
+#endif
