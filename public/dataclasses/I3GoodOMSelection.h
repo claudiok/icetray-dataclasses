ef99b97791ea78357ef4a5a2c23b397eca1cf4e9
Tyce DeYoung <deyoung@phys.psu.edu>
2005-02-03 22:51:53 +0000

    Hid exposed variable goodOMs, provided access functions.
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1277 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3GoodOMSelection.h
+++ public/dataclasses/I3GoodOMSelection.h
@@ -1,11 +1,11 @@
 /**
  * copyright  (C) 2004
  * the IceCube collaboration
- *  $Id: I3GoodOMSelection.h,v 1.6 2005/01/24 19:40:15 deyoung Exp $
+ *  $Id: I3GoodOMSelection.h,v 1.7 2005/02/03 22:51:53 deyoung Exp $
  *
  * @file I3GoodOMSelection.h
- * @version $Revision: 1.6 $
- * @date $Date: 2005/01/24 19:40:15 $
+ * @version $Revision: 1.7 $
+ * @date $Date: 2005/02/03 22:51:53 $
  * @author deyoung
  */
 
@@ -17,12 +17,10 @@
 class I3GoodOMSelection;
 typedef PtrPolicy<I3GoodOMSelection>::ThePolicy I3GoodOMSelectionPtr;
 
-class I3GoodOMSelection : public I3OMResponseSelection 
-{
- public:
-  
-  VectorPolicy<OMKey>::ThePolicy goodOMs;
+class I3GoodOMSelection : public I3OMResponseSelection {
 
+public:
+  
   I3GoodOMSelection() {};
   
   I3GoodOMSelection(const VectorPolicy<OMKey>::ThePolicy &list) 
@@ -42,6 +40,22 @@ class I3GoodOMSelection : public I3OMResponseSelection
       else
 	return false;
     };
+  
+  VectorPolicy<OMKey>::ThePolicy GetGoodOMs() const {
+    return goodOMs;
+  };
+  
+  virtual void SetGoodOMs(const VectorPolicy<OMKey>::ThePolicy& good_list) {
+    if (goodOMs.Size() > 0) {
+      log_warn("Overwriting list of good OMs.");
+      goodOMs.Clear();
+    }
+    goodOMs = good_list;
+  }
+  
+  virtual void AddGoodOM(OMKey key) {
+    goodOMs.Add(key);
+  }
 
   virtual I3OMResponseSelectionPtr GetCopy() 
     {
@@ -59,6 +73,10 @@ class I3GoodOMSelection : public I3OMResponseSelection
   }
 
   // default-generated assignment and copy-constructor explicitly allowed
+
+private:
+
+  VectorPolicy<OMKey>::ThePolicy goodOMs;
   
   ClassDef(I3GoodOMSelection,1);
 };
