d98124d3b74c40ca5c2a95a6f6f37075ab77d3f8
Tyce DeYoung <deyoung@phys.psu.edu>
2005-02-04 17:20:02 +0000

    Added functionality for GetSelector() with empty string for name (returns base selector, identical to plain iterator).
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1282 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3OMSelectionDict.h
+++ public/dataclasses/I3OMSelectionDict.h
@@ -1,11 +1,11 @@
 /**
  * copyright  (C) 2005
  * the IceCube Collaboration
- * $Id: I3OMSelectionDict.h,v 1.4 2005/01/26 16:40:14 deyoung Exp $
+ * $Id: I3OMSelectionDict.h,v 1.5 2005/02/04 17:20:02 deyoung Exp $
  *
  * @file I3OMSelectionDict.h
- * @version $Revision: 1.4 $
- * @date $Date: 2005/01/26 16:40:14 $
+ * @version $Revision: 1.5 $
+ * @date $Date: 2005/02/04 17:20:02 $
  * @author deyoung
  * @author troy
  */
@@ -60,11 +60,29 @@ class I3OMSelectionDict : public TObject,
     // selection logic will be deleted when the selector is returned
     // to the calling module, leading to a seg fault.  For now I'll
     // use the original selection, which is fine as long as nobody
-    // changes its state dynamically.
-    I3OMResponseSelectionPtr selection = (*this)[name];
+    // changes its state dynamically. 
+    if (name.empty()) {
+      // I should return a selector based on a base-class
+      // I3OMResponseSelection.  But I need to create it dynamically
+      // and maintain a smart pointer to it to prevent it from going
+      // out of scope.  Once the above issue is resolved, I can just
+      // pop one off and release it, but for now I'll add it to the
+      // map under a hidden name, after first checking whether it's
+      // already there.
+      I3OMSelectionDict::iterator iter = find("_IGNORE_ME_"); 
+      if (iter == end()) {
+	I3OMResponseSelectionPtr dummy(new I3OMResponseSelection);
+	(*this)["_IGNORE_ME_"] = dummy;
+      }
 
-    return I3OMSelector(boost::make_filter_iterator<I3OMResponseSelection>
-			(*selection, map.begin(),map.end()));
+      I3OMResponseSelectionPtr selection = (*this)["_IGNORE_ME_"];
+      return I3OMSelector(boost::make_filter_iterator<I3OMResponseSelection>
+      			  (*selection, map.begin(),map.end()));
+    } else {
+      I3OMResponseSelectionPtr selection = (*this)[name];
+      return I3OMSelector(boost::make_filter_iterator<I3OMResponseSelection>
+			  (*selection, map.begin(),map.end()));
+    }
   }
 
 #endif
