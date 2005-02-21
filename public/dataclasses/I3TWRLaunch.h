03794942a5a7026dde9473f0b7f947d70b10a962
Ralf Ehrlich <ehrlich@icecube.umd.edu>
2005-02-21 06:47:54 +0000

    implemented I3TWRLaunch
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1318 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3TWRLaunch.h
+++ public/dataclasses/I3TWRLaunch.h
@@ -1,51 +1,62 @@
 /**
  * copyright  (C) 2004
  * the icecube collaboration
- * $Id: I3TWRLaunch.h,v 1.1 2004/07/30 15:11:00 blaufuss Exp $
+ * $Id: I3TWRLaunch.h,v 1.2 2005/02/21 06:47:54 ehrlich Exp $
  *
  * @file I3TWRLaunch.h
- * @version $Revision: 1.1 $
- * @date $Date: 2004/07/30 15:11:00 $
- * @author pretz
- * @author blaufuss
- *
+ * @version $Revision: 1.2 $
+ * @date $Date: 2005/02/21 06:47:54 $
+ * @author ehrlich
  */
 #ifndef I3TWRLAUNCH_H
 #define I3TWRLAUNCH_H
 
+#include <TObject.h>
+#include <vector>
 #include "dataclasses/I3DigitalLaunch.h"
-/**
- * @brief Implementation of the AMANDA TWR response
- * @todo This class isn't implemented yet.  It's just holding the place
- */
+
+using namespace std;
+
 class I3TWRLaunch : public I3DigitalLaunch
 {
- public:
+  Int_t           id;
+  Float_t         start_time;
+  Float_t         bin_size;
+  Float_t         baseline;
+  vector<Float_t> digi;
+
+
+  public:
   /**
-   * constructor ... for when there's something to construct
-   */ 
-  I3TWRLaunch() {}
+   * constructor
+   */
+  I3TWRLaunch() {id=0; start_time=0; bin_size=0; baseline=0;}
 
   /**
    * destructor
    */
-  virtual ~I3TWRLaunch(){}
+  virtual ~I3TWRLaunch() {;}
 
- private:
-  // copy and assignment declared private
-  I3TWRLaunch(const I3TWRLaunch&);
-  const I3TWRLaunch& operator=(const I3TWRLaunch&);
+  Int_t            GetId() {return id;}
+  void             SetId(Int_t id_) {id=id_;}
+  Float_t          GetStartTime() {return start_time;}
+  void             SetStartTime(Float_t start_time_) {start_time=start_time_;}
+  Float_t          GetBinSize() {return bin_size;}
+  void             SetBinSize(Float_t bin_size_) {bin_size=bin_size_;}
+  Float_t          GetBaseline() {return baseline;}
+  void             SetBaseline(Float_t baseline_) {baseline=baseline_;}
+  vector<Float_t>& GetDigi() {return digi;}
 
-  //ROOT macro
+  private:
+
+  // ROOT macro
   ClassDef(I3TWRLaunch,1);
 };
 
-
 /**
- * Pointer typedeffed away to insulate users from the 
+ * Pointer typedeffed away to insulate users from the
  * memory-mananagement implementation
  */
 typedef PtrPolicy<I3TWRLaunch>::ThePolicy I3TWRLaunchPtr;
-
 #endif
 
