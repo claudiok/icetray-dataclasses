9438f77cda21a4abc46bbe922b8406c119a8f4e4
Alex Olivas <olivas@icecube.umd.edu>
2005-04-01 01:08:33 +0000

    added serialization test
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1373 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3Energetic.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Energetic.cxx,v 1.1 2005/04/01 01:08:33 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/01 01:08:33 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3Energetic.h"
+
+TEST_GROUP(I3Energetic);
+
+I3_SERIALIZATION_TEST(I3Energetic);
