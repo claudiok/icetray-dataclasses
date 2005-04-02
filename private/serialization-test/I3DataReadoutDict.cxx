d625d8f994b87ba738088295b10f6392badb0732
Troy Straszheim <troy@icecube.umd.edu>
2005-04-02 20:32:56 +0000

    new serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1384 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3DataReadoutDict.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3DataReadoutDict.cxx,v 1.1 2005/04/02 20:32:56 troy Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/02 20:32:56 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3DataReadoutDict.h"
+
+TEST_GROUP(I3DataReadoutDict);
+
+I3_SERIALIZATION_TEST(I3DataReadoutDict);
---
+++ private/serialization-test/I3RecoHitSeries.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3RecoHitSeries.cxx,v 1.1 2005/04/02 20:32:56 troy Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/02 20:32:56 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3RecoHitSeries.h"
+
+TEST_GROUP(I3RecoHitSeries);
+
+I3_SERIALIZATION_TEST(I3RecoHitSeries);
