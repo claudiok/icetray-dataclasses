9ac2aeb83da9aeb622c159518eea72b3fa63f2d0
Alex Olivas <olivas@icecube.umd.edu>
2005-04-02 17:51:01 +0000

    tests for boost serialization
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1382 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3Time.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Time.cxx,v 1.1 2005/04/02 17:51:01 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/02 17:51:01 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3Time.h"
+
+TEST_GROUP(I3Time);
+
+I3_SERIALIZATION_TEST(I3Time);
