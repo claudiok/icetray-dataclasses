6ae85f33ad0415cdf5de433445aabb2c4472dd4e
Alex Olivas <olivas@icecube.umd.edu>
2005-04-06 21:39:59 +0000

    zenith and azimuth don't need to be serialized
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1389 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3Direction.h
+++ public/dataclasses/I3Direction.h
@@ -1,11 +1,11 @@
 /**
  * copyright  (C) 2004
  * the icecube collaboration
- * $Id: I3Direction.h,v 1.12 2005/04/01 22:38:41 olivas Exp $
+ * $Id: I3Direction.h,v 1.13 2005/04/06 21:39:59 olivas Exp $
  *
  * @file I3Direction.h
- * @version $Revision: 1.12 $
- * @date $Date: 2005/04/01 22:38:41 $
+ * @version $Revision: 1.13 $
+ * @date $Date: 2005/04/06 21:39:59 $
  * @author dule
  */
 
@@ -14,7 +14,7 @@
 //   Done similarly to I3Position
 //***********************************************************
 
-// $Id: I3Direction.h,v 1.12 2005/04/01 22:38:41 olivas Exp $
+// $Id: I3Direction.h,v 1.13 2005/04/06 21:39:59 olivas Exp $
 
 #ifndef I3DIRECTION_H
 #define I3DIRECTION_H
@@ -234,11 +234,9 @@ class I3Direction : public TObject
   template <class Archive>
   void serialize(Archive& ar, unsigned version)
   {
-    ar & make_nvp("Zenith", zenith_);
-    ar & make_nvp("Azimuth", azimuth_);
-    ar & make_nvp("XDir", xDir_);
-    ar & make_nvp("YDir", yDir_);
-    ar & make_nvp("ZDir", zDir_);
+        ar & make_nvp("XDir", xDir_);
+        ar & make_nvp("YDir", yDir_);
+        ar & make_nvp("ZDir", zDir_);
   }
 
   // ROOT macro
