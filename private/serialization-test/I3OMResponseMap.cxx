fc79d01bea5b7cc31b9de7894db862a3e87be173
Alex Olivas <olivas@icecube.umd.edu>
2005-04-11 15:24:20 +0000

    serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1419 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3OMResponseMap.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3OMResponseMap.cxx,v 1.1 2005/04/11 15:24:20 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/11 15:24:20 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3OMResponseMap.h"
+
+TEST_GROUP(I3OMResponseMap);
+
+I3_SERIALIZATION_TEST(I3OMResponseMap);
---
+++ private/serialization-test/OMKey.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: OMKey.cxx,v 1.1 2005/04/11 15:24:20 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/11 15:24:20 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/OMKey.h"
+
+TEST_GROUP(OMKey);
+
+I3_SERIALIZATION_TEST(OMKey);
