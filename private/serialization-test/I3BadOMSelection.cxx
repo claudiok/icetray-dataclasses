303d29e269d28df008e87372c67b257f048b9e5d
Alex Olivas <olivas@icecube.umd.edu>
2005-04-12 19:22:43 +0000

    serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1432 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3BadOMSelection.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3BadOMSelection.cxx,v 1.1 2005/04/12 19:22:43 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/12 19:22:43 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3BadOMSelection.h"
+
+TEST_GROUP(I3BadOMSelection);
+
+I3_SERIALIZATION_TEST(I3BadOMSelection);
---
+++ private/serialization-test/I3GoodOMSelection.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3GoodOMSelection.cxx,v 1.1 2005/04/12 19:22:43 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/12 19:22:43 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3GoodOMSelection.h"
+
+TEST_GROUP(I3GoodOMSelection);
+
+I3_SERIALIZATION_TEST(I3GoodOMSelection);
