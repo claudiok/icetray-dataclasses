c1a8b10693429f7666ef0f44d0be0cb1ed69c365
Alex Olivas <olivas@icecube.umd.edu>
2005-04-12 23:12:21 +0000

    boost serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1439 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3Event.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3Event.cxx,v 1.1 2005/04/12 23:12:21 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/12 23:12:21 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3Event.h"
+
+TEST_GROUP(I3Event);
+
+I3_SERIALIZATION_TEST(I3Event);
