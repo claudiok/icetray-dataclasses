af94338875846b1bffba3f65da2fc15cc53fbe10
Alex Olivas <olivas@icecube.umd.edu>
2005-04-12 18:04:57 +0000

    serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1428 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3RecoResultSingleParticle.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3RecoResultSingleParticle.cxx,v 1.1 2005/04/12 18:04:56 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/12 18:04:56 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3RecoResultSingleParticle.h"
+
+TEST_GROUP(I3RecoResultSingleParticle);
+
+I3_SERIALIZATION_TEST(I3RecoResultSingleParticle);
