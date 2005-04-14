f83189f6e1abc3c117a449eb076aefa32e64bce7
Alex Olivas <olivas@icecube.umd.edu>
2005-04-14 17:11:03 +0000

    serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/dataclasses/trunk@1447 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3AMANDAAnalogReadout.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3AMANDAAnalogReadout.cxx,v 1.1 2005/04/14 17:11:03 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/14 17:11:03 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3AMANDAAnalogReadout.h"
+
+TEST_GROUP(I3AMANDAAnalogReadout);
+
+I3_SERIALIZATION_TEST(I3AMANDAAnalogReadout);
---
+++ private/serialization-test/I3DigitalReadout.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3DigitalReadout.cxx,v 1.1 2005/04/14 17:11:03 olivas Exp $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005/04/14 17:11:03 $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3DigitalReadout.h"
+
+TEST_GROUP(I3DigitalReadout);
+
+I3_SERIALIZATION_TEST(I3DigitalReadout);
