1309cf5f1c39303a9edebc6f021ea0452cd4680e
Alex Olivas <olivas@icecube.umd.edu>
2005-04-20 19:41:59 +0000

    serialization tests
    git-svn-id: http://code.icecube.wisc.edu/svn/trunk/dataclasses@6431 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3EventHeader.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3EventHeader.cxx 1382 2005-04-02 17:51:01Z olivas $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005-04-02 12:51:01 -0500 (Sat, 02 Apr 2005) $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3EventHeader.h"
+
+TEST_GROUP(I3EventHeader);
+
+I3_SERIALIZATION_TEST(I3EventHeader);
