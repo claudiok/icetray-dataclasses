384a5f97610ea4086b37f572d961ebf5d5d29aec
Erik Blaufuss <blaufuss@icecube.umd.edu>
2005-04-27 13:40:50 +0000

    Tests for RecoPulse serialization.  Once the tests are fixed that is
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7001 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test/I3RecoPulseSeries.cxx
@@ -0,0 +1,17 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3RecoPulseSeries.cxx 1384 2005-04-02 20:32:56Z troy $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005-04-02 15:32:56 -0500 (Sat, 02 Apr 2005) $
+    @author Troy D. Straszheim
+
+*/
+
+#include "serialization-test.h"
+#include "dataclasses/I3RecoPulseSeries.h"
+
+TEST_GROUP(I3RecoPulseSeries);
+
+I3_SERIALIZATION_TEST(I3RecoPulseSeries);
