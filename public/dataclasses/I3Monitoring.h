52f97aed5d9bc503f9bf499ecc1da8409a9dbcfc
John Pretz <pretz@icecube.umd.edu>
2005-05-03 16:04:11 +0000

    starting in on a Monitoring stream for Ignacio
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7201 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3Monitoring.h
@@ -0,0 +1,14 @@
+#ifndef I3MONITORING_H
+#define I3MONITORING_H
+
+/**
+ * @brief Stub implementation to get Ignacio started
+ */
+class I3Monitoring 
+{
+
+};
+
+typedef shared_ptr<I3Monitoring> I3MonitoringPtr;
+
+#endif
---
+++ public/dataclasses/I3MonitoringHeader.h
@@ -0,0 +1,16 @@
+#ifndef I3MONITORINGHEADER_H
+#define I3MONITORINGHEADER_H
+
+#include "dataclasses/I3DataHeader.h"
+
+/**
+ * @brief 
+ */
+class I3MonitoringHeader : public I3DataHeader
+{
+
+};
+
+typedef shared_ptr<I3MonitoringHeader> I3MonitoringHeaderPtr;
+
+#endif
