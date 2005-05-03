6378e016304350238f07c347605866595af47bc1
Troy Straszheim <troy@icecube.umd.edu>
2005-05-03 16:03:48 +0000

    patches against libstdc++, so that NaN serializes in from XML correctly on platforms that have buggy libstdc++'es: some places will barf when you try to stream the string "nan" in to a float or double. This patch checks explicitly for a starting "n" or "N", sets NAN, and eats the string.
    Also more thorough specific tests for bools.  Not sure where the
    problems with these are at the moment.
    
    
    
    
    
    
    
    
    
    
    
    
    
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7199 16731396-06f5-0310-8873-f7f720988828

--- private/serialization-test/HasBool.cxx
+++ private/serialization-test/HasBool.cxx
@@ -24,3 +24,35 @@ struct HasBool {
 TEST_GROUP(HasBool);
 
 I3_SERIALIZATION_TEST(HasBool);
+
+TEST(verified_bool_test)
+{
+  HasBool b;
+  std::ofstream ofs("/tmp/verified_bool_test.xml");
+  {
+    xml_oarchive oa(ofs);
+    b.b = true;
+    oa << make_nvp("HasTrueBool", b);
+  }
+  {
+    xml_oarchive oa(ofs);
+    b.b = false;
+    oa << make_nvp("HasFalseBool", b);
+  }
+  ofs.close();
+  std::ifstream ifs("/tmp/verified_bool_test.xml");
+  
+  {
+    b.b = false;
+    xml_iarchive ia(ifs);
+    ia >> make_nvp("HasTrueBool", b);
+    ENSURE(b.b == true);
+  }
+  {
+    xml_iarchive ia(ifs);
+    ia >> make_nvp("HasFalseBool", b);
+    ENSURE(b.b == false);
+  }
+  ifs.close();
+}
+
--- private/serialization-test/HasNAN.cxx
+++ private/serialization-test/HasNAN.cxx
@@ -11,6 +11,7 @@
 
 #include "icetray/test/serialization-test.h"
 #include "dataclasses/I3Hit.h"
+#include <sstream>
 
 struct HasNan {
   float f;
@@ -32,3 +33,72 @@ struct HasNan {
 TEST_GROUP(HasNan);
 
 I3_SERIALIZATION_TEST(HasNan);
+
+// this test shows if streaming in a float as "nan" from text actually
+// works or bails out.  The behavior is in libstdc++, not in our stuff.
+#if 0
+TEST(stdio_nan)
+{
+  ostringstream oss;
+  float f = NAN;
+  oss << f;
+  istringstream iss(oss.str());
+  cout << "oss.str == " << oss.str() << endl;
+  cout << "iss.str == " << iss.str() << endl;
+  //  ENSURE(iss.str() == "nan");
+  f = 2;
+  ENSURE(!iss.fail());
+  iss >> f;
+  ENSURE(!iss.fail());
+  cout << "f=" << f << endl;
+  ENSURE(isnan(f));
+}
+#endif
+
+
+TEST(a_nan)
+{
+  double d = NAN;
+  std::ofstream ofs("/tmp/one_nan.xml");
+  xml_oarchive oa(ofs);
+  oa << make_nvp("a_nan", d);
+  ofs.close();
+  std::ifstream ifs("/tmp/one_nan.xml");
+  xml_iarchive ia(ifs);
+  ia >> make_nvp("a_nan", d);
+  ENSURE(isnan(d));
+  ifs.close();
+}
+
+TEST(verified_nan_test)
+{
+  HasNan n;
+  std::ofstream ofs("/tmp/verified_nan_test.xml");
+  {
+    xml_oarchive oa(ofs);
+    oa << make_nvp("HasNans", n);
+  }
+  {
+    xml_oarchive oa(ofs);
+    n.f = 1.0;
+    n.d = 1.0;
+    oa << make_nvp("HasntNans", n);
+  }
+
+  ofs.close();
+  std::ifstream ifs("/tmp/verified_nan_test.xml");
+  
+  {
+    xml_iarchive ia(ifs);
+    ia >> make_nvp("HasNans", n);
+    ENSURE(isnan(n.d));
+    ENSURE(isnan(n.f));
+  }
+  {
+    xml_iarchive ia(ifs);
+    ia >> make_nvp("HasntNans", n);
+    ENSURE(n.d== 1.0);
+    ENSURE(n.f== 1.0);
+  }
+  ifs.close();
+}
