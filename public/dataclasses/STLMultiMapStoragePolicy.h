8ba364017ff2b59c4364e63209a520d668d3f45f
Alex Olivas <olivas@icecube.umd.edu>
2005-05-03 14:37:00 +0000

    serialization methods for MultiMaps and tests
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7192 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/STLMultiMapStoragePolicy.h
+++ public/dataclasses/STLMultiMapStoragePolicy.h
@@ -1,11 +1,11 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: STLMultiMapStoragePolicy.h,v 1.5 2005/01/24 23:17:44 ehrlich Exp $
+    $Id$
 
     @file STLMultiMapStoragePolicy.h
     @version $Revision: 1.5 $
-    @date $Date: 2005/01/24 23:17:44 $
+    @date $Date$
     @author Ralf Ehrlich
 
 */
@@ -298,6 +298,18 @@ class STLMultiMapStoragePolicy {
    * same as Count()
    */
   size_type count(const KeyType& key) const {return map_.count(key);}
+
+
+ private:
+
+  friend class boost::serialization::access;
+
+  template <class Archive>
+  void serialize(Archive& ar, unsigned version)
+  {
+    ar & make_nvp("map", map_);
+  }
+
 };
 
 #endif
