327fc6bcd31b5248e8154b10d8e939458226fc08
Dawn Williams <drwilliams3@ua.edu>
2005-05-05 17:50:09 +0000

    added methods for doing voltage conversion and pedestal subtraction separately.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7297 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3DOMCalibration.h
+++ public/dataclasses/I3DOMCalibration.h
@@ -93,7 +93,31 @@ public:
   double GetATWDGain(int channel);
 
   double GetATWDGainErr(int channel);
-  
+
+  /**
+   * Returns the ATWD trace in ATWD counts, not converted to
+   * voltage, with the pedestal pattern subtracted 
+   */
+
+  int GetPedestalSubtracted(int id,  
+		     int channel, 
+		     int bin, 
+		     double fe_pedestal,
+		     int count);
+
+
+  /**
+   * Converts the pedestal-subtracted ATWD trace from
+   * ATWD counts to voltage.
+   *
+   */
+
+  double GetPedestalSubtractedVoltage(int id, 
+			      int channel, 
+			      int bin, 
+			      double fe_pedestal,
+			      int count);
+
   /**
    * Return the voltage value corresponding to the count 
    * for a specific ATWD id, channel, and bin.  
