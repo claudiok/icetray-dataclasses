1caec8b103801766c07146db408f666c40c466f6
Ignacio Taboada <itaboada@gatech.edu>
2005-05-05 20:14:41 +0000

    Implementation of I3MonitoringHeader
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7311 16731396-06f5-0310-8873-f7f720988828

---
+++ private/dataclasses/I3MonitoringHeader.cxx
@@ -0,0 +1,61 @@
+#include "dataclasses/I3MonitoringHeader.h"
+
+int I3MonitoringHeader::GetSize()
+{
+    return size_;
+}
+
+int I3MonitoringHeader::GetType()
+{
+    return type_;
+}
+
+OMKey I3MonitoringHeader::GetOMKey()
+{
+    return om_;
+}
+
+long long int I3MonitoringHeader::GetMBID()
+{
+    return mbid_;
+}
+
+int I3MonitoringHeader::GetRun()
+{
+    return run_;
+}
+
+I3Time I3MonitoringHeader::GetTime()
+{
+    return record_time_;
+}
+
+void I3MonitoringHeader::SetSize(int s)
+{
+    size_ = s; 
+}
+
+void I3MonitoringHeader::SetType(int t)
+{
+    type_ = t;
+}
+
+void I3MonitoringHeader::SetOMKey(OMKey ok)
+{
+    om_ = ok;
+}
+
+void I3MonitoringHeader::SetMBID(long long int id)
+{
+    mbid_ = id;
+}
+
+void I3MonitoringHeader::SetRun(int r)
+{
+    run_ = r;
+}
+
+void I3MonitoringHeader::SetTime(I3Time t)
+{
+    record_time_ = t;
+}
