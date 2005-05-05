5142233388a991267279cb55e5ae99c6165b3282
Ignacio Taboada <itaboada@gatech.edu>
2005-05-05 20:13:57 +0000

    I've created a minimalistic set of members for I3MonitoringHeader. Mostly getters and setters. Private members currently consist of the minimal description of a Monitoring record.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7310 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3MonitoringHeader.h
+++ public/dataclasses/I3MonitoringHeader.h
@@ -1,14 +1,46 @@
+/**
+ * copyright  (C) 2005
+ * the icecube collaboration
+ *
+ * @file I3MonitoringHeader.h
+ * @author pretz
+ * @author Taboada
+ */
+
 #ifndef I3MONITORINGHEADER_H
 #define I3MONITORINGHEADER_H
 
 #include "dataclasses/I3DataHeader.h"
+#include "dataclasses/OMKey.h"
 
 /**
- * @brief 
+ * @brief This class defines the entries in the header of a monitoring record
+ * in a .mon file as produced by monolith.
  */
 class I3MonitoringHeader : public I3DataHeader
 {
+ public:
+    int GetSize();
+    int GetType();
+    OMKey GetOMKey();
+    long long int GetMBID();
+    int GetRun();
+    I3Time GetTime();
 
+    void SetSize(int);
+    void SetType(int);
+    void SetOMKey(OMKey);
+    void SetMBID(long long int);
+    void SetRun(int);
+    void SetTime(I3Time);
+
+ private:
+    OMKey om_;
+    long long int mbid_;
+    int size_;
+    int type_;
+    int run_;
+    I3Time record_time_;
 };
 
 typedef shared_ptr<I3MonitoringHeader> I3MonitoringHeaderPtr;
