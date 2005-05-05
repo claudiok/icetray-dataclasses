1ececb8eb955b9a2a4503b97e0ed81415eaaa96f
Ignacio Taboada <itaboada@gatech.edu>
2005-05-05 18:58:01 +0000

    I've added a minimalistic set of members to I3Monitoring. Public setter and getters; and the minimal private members: temperature, SPE Scaler and MPE Scaler
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7303 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3Monitoring.h
+++ public/dataclasses/I3Monitoring.h
@@ -1,12 +1,42 @@
+/**
+ * copyright  (C) 2005
+ * the icecube collaboration
+ *
+ * @file I3Monitoring.h
+ * @author pretz
+ * @author Taboada
+ */
+
 #ifndef I3MONITORING_H
 #define I3MONITORING_H
 
 /**
- * @brief Stub implementation to get Ignacio started
+ * @brief Class to represent the contents of DOM monitoring records
  */
+
 class I3Monitoring 
 {
+ public:
+    float GetTempCelcius();
+    int GetTempRaw();
+    float GetSPEScalerHz();
+    int GetSPEScalerRaw();
+    float GetMPEScalerHz();
+    int GetMPEScalerRaw();
 
+    void SetTemperatureRaw(int);
+    void SetSPEScalerRaw(int);
+    void SetMPEScalerRaw(int);
+
+ private:
+    /** I store the temperature, SPE_Scaler and MPE_Scaler in the
+	DAQ format.
+	Presently Temp is an unsigned int from 0 to 65535. 
+	SPE_Scaler and MPE_scaler are counts per 0.9 seconds.
+     */
+    int temperature_;
+    int SPE_Scaler_;
+    int MPE_Scaler_;
 };
 
 typedef shared_ptr<I3Monitoring> I3MonitoringPtr;
