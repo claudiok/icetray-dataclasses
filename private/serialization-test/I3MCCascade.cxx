d5e18928cd4d2f6eba15c659ac750c34b46d0c70
Alex Olivas <olivas@icecube.umd.edu>
2005-05-14 18:55:49 +0000

    serialization tests for I3MCTrack and I3MCCascade
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7621 16731396-06f5-0310-8873-f7f720988828

--- private/serialization-test/I3MCCascade.cxx
+++ private/serialization-test/I3MCCascade.cxx
@@ -1,7 +1,7 @@
 /**
     copyright  (C) 2004
     the icecube collaboration
-    $Id: I3MCTrack.cxx 7032 2005-04-27 21:44:45Z olivas $
+    $Id: I3MCCascade.cxx 7032 2005-04-27 21:44:45Z olivas $
 
     @version $Revision: 1.1 $
     @date $Date: 2005-04-27 17:44:45 -0400 (Wed, 27 Apr 2005) $
@@ -10,14 +10,15 @@
 */
 
 #include "icetray/test/serialization-test.h"
-#include "dataclasses/I3MCTrack.h"
+#include "dataclasses/I3MCCascade.h"
 #include "dataclasses/I3InIceParticle.h"
 #include "dataclasses/I3Particle.h"
 
-TEST_GROUP(I3MCTrack);
+TEST_GROUP(I3MCCascade);
 
-I3_SERIALIZATION_TEST(I3MCTrack);
+I3_SERIALIZATION_TEST(I3MCCascade);
 
-I3_VIRTUAL_SERIALIZATION_TEST(I3InIceParticle,I3MCTrack);
+I3_VIRTUAL_SERIALIZATION_TEST(I3InIceParticle,I3MCCascade);
 
-I3_VIRTUAL_SERIALIZATION_TEST(I3Particle,I3MCTrack);
+I3_VIRTUAL_SERIALIZATION_TEST(I3Particle,I3MCCascade);
+
