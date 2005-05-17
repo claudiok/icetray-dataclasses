c25ed4379c42e370fe16570fb00cdf59084430a0
Alex Olivas <olivas@icecube.umd.edu>
2005-05-17 21:35:07 +0000

    I3RecoResultRDMCFit needs its own serialization directory for now. it inherits from a class that also needs to be serialized through the same base class shared pointer.  on linking the two boost export macros clash.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@7705 16731396-06f5-0310-8873-f7f720988828

---
+++ private/serialization-test-special/main.cxx
@@ -0,0 +1,20 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: main.cxx 7468 2005-05-09 20:09:11Z ehrlich $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2005-05-09 16:09:11 -0400 (Mon, 09 May 2005) $
+    @author Troy D. Straszheim
+
+    @todo
+
+    This simply includes the test driver program from the TUT tool.
+    If you wish to customize this program you can replace this file's
+    contents with your modified version.
+
+*/
+
+#include <icetray/test/main.ixx>
+
+
