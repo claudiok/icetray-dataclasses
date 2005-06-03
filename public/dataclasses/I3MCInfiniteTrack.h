18aa93cf0c888ad4a68889f8639fe9133ac341e5
Ralf Ehrlich <ehrlich@icecube.umd.edu>
2005-06-03 20:47:32 +0000

    added this class for infinite MC tracks (these tracks can be found in some f2k files)
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@8861 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3MCInfiniteTrack.h
@@ -0,0 +1,77 @@
+/**
+    copyright  (C) 2004
+    the IceCube collaboration
+    $Id: I3MCTrack.h 7619 2005-05-14 18:52:55Z olivas $
+
+    @file I3MCTrack.h
+    @version $Revision: 1.14 $
+    @date $Date: 2005-05-14 14:52:55 -0400 (Sat, 14 May 2005) $
+    @author deyoung
+*/
+
+#include "I3TrackImpl.h"
+#include "I3Infinite.h"
+#include "I3Composite.h"
+#include "I3Energetic.h"
+
+#ifndef I3MCINFINITETRACK_H
+#define I3MCINFINITETRACK_H
+
+#include "dataclasses/I3Track.h"
+
+/**
+ * @brief For representing infinite tracks from Monte Carlo, where we have all
+ * possible information. There are F2k files which have infinite MC tracks, which are not primaries.
+ */
+class I3MCInfiniteTrack : public I3TrackImpl<I3Infinite,
+                                             I3Energetic,
+                                             I3Composite>
+{
+  double     weight_;
+  
+  public:
+  /**
+   * constructor
+   */
+  I3MCInfiniteTrack() {weight_=1;};
+
+  /**
+   * destructor
+   */
+  virtual ~I3MCInfiniteTrack(){}
+  
+  /**
+   * gives the weight of the track
+   */
+  double GetWeight() const { return weight_; }
+
+  /**
+   * sets the weight of the track
+   */
+  void  SetWeight(double weight) { weight_ = weight; }
+
+ private:
+
+  friend class boost::serialization::access;
+
+  template <class Archive>
+  void serialize(Archive& ar, unsigned version)
+  {
+    ar & make_nvp("I3MCInfiniteTrack", base_object< I3TrackImpl<I3Infinite,
+                                                          I3Energetic,
+	                                                  I3Composite> >(*this));
+    ar & make_nvp("Weight", weight_ );
+  }
+
+  //ROOT macro
+  ClassDef(I3MCInfiniteTrack,1);
+};
+
+BOOST_SHARED_POINTER_EXPORT(I3MCInfiniteTrack);
+
+/** 
+ * pointer type to insulate users from memory management
+ */
+typedef shared_ptr<I3MCInfiniteTrack>  I3MCInfiniteTrackPtr;
+
+#endif
