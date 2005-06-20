a723eb6eb6231f9b9c216175c84d0c07772b4c9d
John Pretz <pretz@icecube.umd.edu>
2005-06-20 22:36:05 +0000

    tut->I3Test
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@9329 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3CalibrationTest.cxx
+++ private/test/I3CalibrationTest.cxx
@@ -1,4 +1,4 @@
-#include "TUT/tut.h"
+#include <I3Test.h>
 
 #include "dataclasses/I3Calibration.h"
 #include "dataclasses/I3InIceCalibration.h"
@@ -12,108 +12,91 @@ using std::string;
 using std::cout;
 using std::endl;
 
-namespace tut
-{
-    struct I3CalibrationTest
-    {};
+TEST_GROUP(I3CalibrationTest);
+
+// Test file I/O
+TEST(file_io)
+{	
+  try
+    {
+      I3Calibration* calib = new I3Calibration();
+      //	    calib->ls();
+      
+      I3InIceCalibration& inice_calib = calib->GetInIceCalibration();
+      //	    inice_calib.ls();
+      
+      I3DOMCalibPtr dom_calib(new I3DOMCalibration);
+      //	    dom_calib->ls();
+      
+      int id = 0;
+      int channel = 0;
+      int bin = 127;
+      
+      double slope = -0.002;
+      double intercept = 2.9; 
+      double regress_coeff = 0.99;
+      double gain = -17.0;
+      
+      double val;
+      double gainErr = 0.0;
+      
+      val = slope;
+      
+      dom_calib->SetATWDGain(channel,gain,gainErr);
+      dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
+      
+      inice_calib[OMKey(20,20)] = dom_calib;
+      
+      I3DOMCalibPtr domptr = inice_calib[OMKey(20,20)];
+      
+      ENSURE_DISTANCE(gain, domptr->GetATWDGain(0), 0.0001,
+		      "Failed to get gain from I3DOMCalibration "
+		      "read from disk");
+      ENSURE_DISTANCE(intercept, 
+		      domptr->GetATWDVoltage(0, 0,0,0.0, 0)*gain, 
+		      0.0001,
+		      "Failed to get voltage from "
+		      "I3DOMCalibration read from disk");
+    }
   
-  typedef test_group<I3CalibrationTest> factory;
-  typedef factory::object object;
+  catch(exception& e)
+    {
+      ENSURE("No exceptions thrown ",0);
+    }
 }
 
-namespace
-{
-  static tut::factory t("I3CalibrationTest");
-}
-
-namespace tut 
-{
-  // Test file I/O
-  template<> template<>
-  void object::test<1>()
-  {	
-    try
-      {
-	I3Calibration* calib = new I3Calibration();
-	//	    calib->ls();
-	
-	I3InIceCalibration& inice_calib = calib->GetInIceCalibration();
-	//	    inice_calib.ls();
-	
-	I3DOMCalibPtr dom_calib(new I3DOMCalibration);
-	//	    dom_calib->ls();
-	
-	int id = 0;
-	int channel = 0;
-	int bin = 127;
-	
-	double slope = -0.002;
-	double intercept = 2.9; 
-	double regress_coeff = 0.99;
-	double gain = -17.0;
-	
-	double val;
-	double gainErr = 0.0;
-
-	val = slope;
-	
-	dom_calib->SetATWDGain(channel,gain,gainErr);
-	dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
-	
-	inice_calib[OMKey(20,20)] = dom_calib;
-	
-	I3DOMCalibPtr domptr = inice_calib[OMKey(20,20)];
-	
-	ensure_distance("Failed to get gain from I3DOMCalibration "
-			"read from disk", 
-			gain, domptr->GetATWDGain(0), 0.0001);
-	cout<<"LOOK: "<<domptr->GetATWDVoltage(0,0,0,0.0,0);
-	ensure_distance("Failed to get voltage from "
-			"I3DOMCalibration read from disk",
-			intercept, 
-			domptr->GetATWDVoltage(0, 0,0,0.0, 0)*gain, 
-			0.0001);
-      }
-    
-    catch(exception& e)
-      {
-	ensure("No exceptions thrown ",0);
-      }
-  }
-  
   
   // Test I/O streams
-  template<> template<>
-  void object::test<3>()
-  {	
-    try
-      {
-	I3DOMCalibPtr dom_calib(new I3DOMCalibration);
-	
-	int id = 0;
-	int channel = 0;
-	int bin = 128;
-	
-	double slope = -0.002;
-	double intercept = 2.9; 
-	double regress_coeff = 0.99;
-	double gain = -17.0;
-	
-	double val;
-	double gainErr = 0.0;
-	
-	val = slope;
-	
-	dom_calib->SetATWDGain(channel,gain,gainErr);
-	dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
-	
-	dom_calib->ToStream(cout);
-	cout<< dom_calib;
-      }
-    
-    catch(exception& e)
-      {
-	ensure("No exceptions thrown",0);
-      }
-  }
+TEST(to_stream)
+{	
+  try
+    {
+      I3DOMCalibPtr dom_calib(new I3DOMCalibration);
+      
+      int id = 0;
+      int channel = 0;
+      int bin = 128;
+      
+      double slope = -0.002;
+      double intercept = 2.9; 
+      double regress_coeff = 0.99;
+      double gain = -17.0;
+      
+      double val;
+      double gainErr = 0.0;
+      
+      val = slope;
+      
+      dom_calib->SetATWDGain(channel,gain,gainErr);
+      dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
+      
+      dom_calib->ToStream(cout);
+      cout<< dom_calib;
+    }
+  
+  catch(exception& e)
+    {
+      ENSURE("No exceptions thrown",0);
+    }
 }
+
--- private/test/I3PositionTest.cxx
+++ private/test/I3PositionTest.cxx
@@ -10,7 +10,7 @@
     @todo
 */
 
-#include "TUT/tut.h"
+#include <I3Test.h>
 
 #include "dataclasses/I3Position.h"
 #include "TFile.h"
@@ -20,199 +20,188 @@ using std::string;
 using std::cout;
 using std::endl;
 
-namespace tut
-{
-  struct I3PositionTest
-  {
-  };
+TEST_GROUP(I3PositionTest);
 
-  typedef test_group<I3PositionTest> factory;
-  typedef factory::object object;
+
+/**
+ * check that constructor and destructor work
+ */
+TEST(constructor)
+{
+  I3PositionPtr ptr(new I3Position());
 }
 
-namespace
+/**
+ * check writing object to disk
+ */
+TEST(write)
 {
-  static tut::factory t("I3PositionTest");
-}
-
-namespace tut
-{ 
-  /**
-   * check that constructor and destructor work
-   */
-  void object::test<1>() 
-  {
-     I3PositionPtr ptr(new I3Position());
-  }
+  I3PositionPtr ptr(new I3Position());
   
-  /**
-   * check writing object to disk
-   */
-  void object::test<2>()
-  {
-     I3PositionPtr ptr(new I3Position());
+  /** @todo should fill some data for I3Position here */
+  
+  TFile f_out("test.out.root","RECREATE");
+  ptr->Write();
+  f_out.Close();
+  
+  TFile f_in("test.out.root");
+  TObject* object_in =
+    f_in.FindObjectAny("I3Position");
+  f_in.Close();
+  ENSURE(object_in,"the read-in pointer is non-zero");
+  
+  I3Position* tested_in = dynamic_cast<I3Position*>(object_in);
+  ENSURE(tested_in,"the read-in pointer is of the right type");
+  
+  I3PositionPtr ptr_in(tested_in);
+  
+  /** @todo should check the read-in data for I3Position here */
 
-     /** @todo should fill some data for I3Position here */
-
-     TFile f_out("test.out.root","RECREATE");
-     ptr->Write();
-     f_out.Close();
-
-     TFile f_in("test.out.root");
-     TObject* object_in =
-		f_in.FindObjectAny("I3Position");
-     f_in.Close();
-     ensure("the read-in pointer is non-zero",object_in);
-     
-     I3Position* tested_in = dynamic_cast<I3Position*>(object_in);
-     ensure("the read-in pointer is of the right type",tested_in);
-
-     I3PositionPtr ptr_in(tested_in);
-    
-    /** @todo should check the read-in data for I3Position here */
-
-  }
-   
-   /**
-    * check branching it to a tree
-    */
-   void object::test<3>(){
-   I3PositionPtr ptr(new I3Position());
-   /** @todo add some setting of data here */
- 
-   I3Position* raw_ptr = &(*ptr);
-   TTree tree("TestTree","Icecube");
-   tree.Branch("branch","I3Position",&raw_ptr);
-   
-   tree.Fill();
-
-   I3Position* raw_ptr_in = new I3Position();
-   I3PositionPtr ptr_in (raw_ptr_in);
-
-   tree.SetBranchAddress("branch",&raw_ptr_in);
-   tree.GetEvent(0);
-
-   /** @todo add some checking of the data of I3Position here */
-
-   }
-
-   /**
-    * check branching object to a tree, writing the tree to disk,
-    * and reading the tree back in, and finding the object in the tree.
-    */
-   void object::test<4>(){
-   I3PositionPtr ptr(new I3Position());
-   /** @todo add some setting of data here */
-
-   I3Position* raw_ptr = &(*ptr);
-   TTree tree("TestTree","Icecube");
-   tree.Branch("branch","I3Position",&raw_ptr);
-   tree.Fill();
-
-   TFile file_out("test.out.root","RECREATE");
-   tree.Write();
-   file_out.Close();
-                    
-
-   TFile file_in("test.out.root");
-   TTree* tree_in = (TTree*)file_in.FindObjectAny("TestTree");
-   ensure("the test tree exists",tree_in);
-                                                            
-   I3Position* raw_ptr_in = new I3Position();
-   I3PositionPtr ptr_in (raw_ptr_in);
-
-   tree_in->SetBranchAddress("branch",&raw_ptr_in);
-   tree_in->GetEvent(0);
-
-   /** @todo add some checking of the data of I3Position here */
-
-   delete tree_in;
-   }
-
-  /**
-   * Make sure that the changing of coordinate systems works well
-   */
-  void object::test<5>(){
-    cout <<"Creating positions p(4,3,0) and q(0,0,0)..."<<endl;
-    I3Position p(4,3,0);
-    //I3PositionPtr q_ptr(new I3Position(1,2,3));
-    I3Position q(1,2,3);
-    cout <<"Setting position q to: 2,2,2 in car..."<<endl;
-    q.SetPosition(2,2,2,I3Position::car);
-
-    ensure_distance("p.GetX failed",p.GetX(),4.0,0.0001);
-    ensure_distance("p.GetY failed",p.GetY(),3.0,0.0001);
-    ensure_distance("p.GetZ failed",p.GetZ(),0.0,0.0001);
-    ensure_distance("p.GetR failed",p.GetR(),5.0,0.0001);
-    ensure_distance("p.GetTheta failed",p.GetTheta(),1.5708,0.0001);
-    ensure_distance("p.GetPhi failed",p.GetPhi(),0.6435,0.0001);
-    ensure_distance("p.GetRho failed",p.GetRho(),5.0,0.0001);
-
-    ensure_distance("q.GetX failed",q.GetX(),2.0,0.0001);
-    ensure_distance("q.GetY failed",q.GetY(),2.0,0.0001);
-    ensure_distance("q.GetZ failed",q.GetZ(),2.0,0.0001);
-
-    ensure_distance("p.CalcDistance(q) failed",p.CalcDistance(q),3.0,0.0001);
-    ensure_distance("q.CalcDistance(p) failed",q.CalcDistance(p),3.0,0.0001);
-
-    cout <<"Rotating p by Pi/4 around z-axis..."<<endl;
-    p.RotateZ(3.141592/4);
-    ensure_distance("p.GetX failed",p.GetX(),0.707108,0.0001);
-    ensure_distance("p.GetY failed",p.GetY(),4.94975,0.0001);
-    ensure_distance("p.GetZ failed",p.GetZ(),0.0,0.0001);
-
-    cout <<"Rotating p by Pi/2 around y-axis..."<<endl;
-    p.RotateY(3.141592/2);
-    ensure_distance("p.GetX failed",p.GetX(),0.0,0.0001);
-    ensure_distance("p.GetY failed",p.GetY(),4.94975,0.0001);
-    ensure_distance("p.GetZ failed",p.GetZ(),-0.707108,0.0001);
-
-    cout <<"Rotating p by Pi/2 around x-axis..."<<endl;
-    p.RotateX(3.141592/2);
-    ensure_distance("p.GetX failed",p.GetX(),0.0,0.0001);
-    ensure_distance("p.GetY failed",p.GetY(),0.707108,0.0001);
-    ensure_distance("p.GetZ failed",p.GetZ(),4.94975,0.0001);
-
-    cout <<"Creating position s and setting coordinates in sph..."<<endl;
-    I3Position s;
-    s.SetPosition(1.732050808,0.955316618,3.141592/4,I3Position::sph);
-    ensure_distance("s.GetX failed",s.GetX(),1.0,0.0001);
-    ensure_distance("s.GetY failed",s.GetY(),1.0,0.0001);
-    ensure_distance("s.GetZ failed",s.GetZ(),1.0,0.0001);
-
-    cout <<"Creating position d and setting coordinates in car..."<<endl;
-    I3Position d(1,1,1);
-    ensure_distance("d.GetR failed",d.GetR(),1.73205,0.0001);
-    ensure_distance("d.GetTheta failed",d.GetTheta(),0.955317,0.0001);
-    ensure_distance("d.GetPhi failed",d.GetPhi(),0.785398,0.0001);
-
-    cout <<"Using the = operator on I3Position e=d..."<<endl;
-    I3Position e = d;
-    ensure_distance("e.GetR failed",e.GetR(),1.73205,0.0001);
-    ensure_distance("e.GetTheta failed",e.GetTheta(),0.955317,0.0001);
-    ensure_distance("e.GetPhi failed",e.GetPhi(),0.785398,0.0001);
-
-    cout <<"Creating position f from position d..."<<endl;
-    I3Position f(d);
-    ensure_distance("f.GetX failed",f.GetX(),1.0,0.0001);
-    ensure_distance("f.GetY failed",f.GetY(),1.0,0.0001);
-    ensure_distance("f.GetZ failed",f.GetZ(),1.0,0.0001);
-
-    cout <<"Shifting coordinate system of f by s..."<<endl;
-    f.ShiftCoordSystem(s);
-    ensure_distance("shifted f.GetX failed",f.GetX(),0.0,0.0001);
-    ensure_distance("shifted f.GetY failed",f.GetY(),0.0,0.0001);
-    ensure_distance("shifted f.GetZ failed",f.GetZ(),0.0,0.0001);
-
-    cout <<"Shifting coordinate system of f by p..."<<endl;
-    f.ShiftCoordSystem(p);
-    ensure_distance("shifted f.GetX failed",f.GetX(),0.0,0.0001);
-    ensure_distance("shifted f.GetY failed",f.GetY(),-0.707108,0.0001);
-    ensure_distance("shifted f.GetZ failed",f.GetZ(),-4.94975,0.0001);
-    ensure_distance("shifted f.GetR failed",f.GetR(),5.0,0.0001);
-    ensure_distance("shifted f.GetTheta failed",f.GetTheta(),2.9997,0.0001);
-    ensure_distance("shifted f.GetPhi failed",f.GetPhi(),4.71239,0.0001);
-    ensure_distance("shifted f.GetRho failed",f.GetRho(),0.707108,0.0001);
-
-  }
 }
+
+/**
+ * check branching it to a tree
+ */
+TEST(branch)
+{
+  I3PositionPtr ptr(new I3Position());
+  /** @todo add some setting of data here */
+  
+  I3Position* raw_ptr = &(*ptr);
+  TTree tree("TestTree","Icecube");
+  tree.Branch("branch","I3Position",&raw_ptr);
+  
+  tree.Fill();
+  
+  I3Position* raw_ptr_in = new I3Position();
+  I3PositionPtr ptr_in (raw_ptr_in);
+  
+  tree.SetBranchAddress("branch",&raw_ptr_in);
+  tree.GetEvent(0);
+  
+  /** @todo add some checking of the data of I3Position here */
+  
+}
+
+/**
+ * check branching object to a tree, writing the tree to disk,
+ * and reading the tree back in, and finding the object in the tree.
+ */
+TEST(branch_and_write)
+{
+  I3PositionPtr ptr(new I3Position());
+  /** @todo add some setting of data here */
+  
+  I3Position* raw_ptr = &(*ptr);
+  TTree tree("TestTree","Icecube");
+  tree.Branch("branch","I3Position",&raw_ptr);
+  tree.Fill();
+  
+  TFile file_out("test.out.root","RECREATE");
+  tree.Write();
+  file_out.Close();
+  
+  
+  TFile file_in("test.out.root");
+  TTree* tree_in = (TTree*)file_in.FindObjectAny("TestTree");
+  ENSURE(tree_in,"the test tree exists");
+  
+  I3Position* raw_ptr_in = new I3Position();
+  I3PositionPtr ptr_in (raw_ptr_in);
+  
+  tree_in->SetBranchAddress("branch",&raw_ptr_in);
+  tree_in->GetEvent(0);
+
+  /** @todo add some checking of the data of I3Position here */
+  
+  delete tree_in;
+}
+
+/**
+ * Make sure that the changing of coordinate systems works well
+ */
+TEST(coord_transforms)
+{
+  cout <<"Creating positions p(4,3,0) and q(0,0,0)..."<<endl;
+  I3Position p(4,3,0);
+  //I3PositionPtr q_ptr(new I3Position(1,2,3));
+  I3Position q(1,2,3);
+  cout <<"Setting position q to: 2,2,2 in car..."<<endl;
+  q.SetPosition(2,2,2,I3Position::car);
+  
+  ENSURE_DISTANCE(p.GetX(),4.0,0.0001,"p.GetX failed");
+  ENSURE_DISTANCE(p.GetY(),3.0,0.0001,"p.GetY failed");
+  ENSURE_DISTANCE(p.GetZ(),0.0,0.0001,"p.GetZ failed");
+  ENSURE_DISTANCE(p.GetR(),5.0,0.0001,"p.GetR failed");
+  ENSURE_DISTANCE(p.GetTheta(),1.5708,0.0001,"p.GetTheta failed");
+  ENSURE_DISTANCE(p.GetPhi(),0.6435,0.0001,"p.GetPhi failed");
+  ENSURE_DISTANCE(p.GetRho(),5.0,0.0001,"p.GetRho failed");
+  
+  ENSURE_DISTANCE(q.GetX(),2.0,0.0001,"q.GetX failed");
+  ENSURE_DISTANCE(q.GetY(),2.0,0.0001,"q.GetY failed");
+  ENSURE_DISTANCE(q.GetZ(),2.0,0.0001,"q.GetZ failed");
+  
+  ENSURE_DISTANCE(p.CalcDistance(q),3.0,0.0001,"p.CalcDistance(q) failed");
+  ENSURE_DISTANCE(q.CalcDistance(p),3.0,0.0001,"q.CalcDistance(p) failed");
+  
+  cout <<"Rotating p by Pi/4 around z-axis..."<<endl;
+  p.RotateZ(3.141592/4);
+  ENSURE_DISTANCE(p.GetX(),0.707108,0.0001,"p.GetX failed");
+  ENSURE_DISTANCE(p.GetY(),4.94975,0.0001,"p.GetY failed");
+  ENSURE_DISTANCE(p.GetZ(),0.0,0.0001,"p.GetZ failed");
+  
+  cout <<"Rotating p by Pi/2 around y-axis..."<<endl;
+  p.RotateY(3.141592/2);
+  ENSURE_DISTANCE(p.GetX(),0.0,0.0001,"p.GetX failed");
+  ENSURE_DISTANCE(p.GetY(),4.94975,0.0001,"p.GetY failed");
+  ENSURE_DISTANCE(p.GetZ(),-0.707108,0.0001,"p.GetZ failed");
+  
+  cout <<"Rotating p by Pi/2 around x-axis..."<<endl;
+  p.RotateX(3.141592/2);
+  ENSURE_DISTANCE(p.GetX(),0.0,0.0001,"p.GetX failed");
+  ENSURE_DISTANCE(p.GetY(),0.707108,0.0001,"p.GetY failed");
+  ENSURE_DISTANCE(p.GetZ(),4.94975,0.0001,"p.GetZ failed");
+  
+  cout <<"Creating position s and setting coordinates in sph..."<<endl;
+  I3Position s;
+  s.SetPosition(1.732050808,0.955316618,3.141592/4,I3Position::sph);
+  ENSURE_DISTANCE(s.GetX(),1.0,0.0001,"s.GetX failed");
+  ENSURE_DISTANCE(s.GetY(),1.0,0.0001,"s.GetY failed");
+  ENSURE_DISTANCE(s.GetZ(),1.0,0.0001,"s.GetZ failed");
+  
+  cout <<"Creating position d and setting coordinates in car..."<<endl;
+  I3Position d(1,1,1);
+  ENSURE_DISTANCE(d.GetR(),1.73205,0.0001,"d.GetR failed");
+  ENSURE_DISTANCE(d.GetTheta(),0.955317,0.0001,"d.GetTheta failed");
+  ENSURE_DISTANCE(d.GetPhi(),0.785398,0.0001,"d.GetPhi failed");
+  
+  cout <<"Using the = operator on I3Position e=d..."<<endl;
+  I3Position e = d;
+  ENSURE_DISTANCE(e.GetR(),1.73205,0.0001,"e.GetR failed");
+  ENSURE_DISTANCE(e.GetTheta(),0.955317,0.0001,"e.GetTheta failed");
+  ENSURE_DISTANCE(e.GetPhi(),0.785398,0.0001,"e.GetPhi failed");
+  
+  cout <<"Creating position f from position d..."<<endl;
+  I3Position f(d);
+  ENSURE_DISTANCE(f.GetX(),1.0,0.0001,"f.GetX failed");
+  ENSURE_DISTANCE(f.GetY(),1.0,0.0001,"f.GetY failed");
+  ENSURE_DISTANCE(f.GetZ(),1.0,0.0001,"f.GetZ failed");
+  
+  cout <<"Shifting coordinate system of f by s..."<<endl;
+  f.ShiftCoordSystem(s);
+  ENSURE_DISTANCE(f.GetX(),0.0,0.0001,"shifted f.GetX failed");
+  ENSURE_DISTANCE(f.GetY(),0.0,0.0001,"shifted f.GetY failed");
+  ENSURE_DISTANCE(f.GetZ(),0.0,0.0001,"shifted f.GetZ failed");
+  
+  cout <<"Shifting coordinate system of f by p..."<<endl;
+  f.ShiftCoordSystem(p);
+  ENSURE_DISTANCE(f.GetX(),0.0,0.0001,"shifted f.GetX failed");
+  ENSURE_DISTANCE(f.GetY(),-0.707108,0.0001,"shifted f.GetY failed");
+  ENSURE_DISTANCE(f.GetZ(),-4.94975,0.0001,"shifted f.GetZ failed");
+  ENSURE_DISTANCE(f.GetR(),5.0,0.0001,"shifted f.GetR failed");
+  ENSURE_DISTANCE(f.GetTheta(),2.9997,0.0001,"shifted f.GetTheta failed");
+  ENSURE_DISTANCE(f.GetPhi(),4.71239,0.0001,"shifted f.GetPhi failed");
+  ENSURE_DISTANCE(f.GetRho(),0.707108,0.0001,"shifted f.GetRho failed");
+  
+}
+
