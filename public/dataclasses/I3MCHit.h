c986f1f4fe00913cd948506752d2fb8b058c0ec7
Erik Blaufuss <blaufuss@icecube.umd.edu>
2005-08-31 15:05:37 +0000

    Added the requested CherenkovDistance and Get/Set methods.  For use by ROMEO
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@10870 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3MCHit.h
+++ public/dataclasses/I3MCHit.h
@@ -24,18 +24,21 @@
  * This class records the true (simulated) time at which a
  * photoelectron was produced.  In addition to the time, it contains
  * the identity of the track which was responsible for the hit, and a
- * statistical weight to assign to the hit (default 1).
+ * statistical weight to assign to the hit (default 1).  Also, 
+ * CherenkovDistance- direct path distance to the track that generated
+ * this hit.
  */
 class I3MCHit : public I3Hit
 {
   float  weight_;
   int    particleID_;
+  double  cherenkovDistance_;
 
  public:
   /**
    * constructor
    */
-  I3MCHit() : weight_(0), particleID_(0) { }
+  I3MCHit() : weight_(0), particleID_(0), cherenkovDistance_(0) { }
 
   // default copy constructor, assignment operator explicitly used
   /**
@@ -58,6 +61,16 @@ class I3MCHit : public I3Hit
    */
   void SetParticleID(int ParticleID) { particleID_ = ParticleID; }
 
+  /**
+   * @return the direct path distance to the track which caused this hit
+   */
+  double GetCherenkovDistance() const { return cherenkovDistance_; }
+
+  /**
+   * @param CherenkovDistance set the direct path distance to track which caused this hit
+   */
+  void SetParticleID(double CherenkovDistance) { cherenkovDistance_ = CherenkovDistance; }
+
   virtual ~I3MCHit() {} 
 
  private:
@@ -70,6 +83,7 @@ class I3MCHit : public I3Hit
     ar & make_nvp("I3Hit", base_object<I3Hit>(*this));
     ar & make_nvp("Weight", weight_);
     ar & make_nvp("ParticleID", particleID_);
+    ar & make_nvp("CherenkovDistance", cherenkovDistance_);
   }     
 
   ClassDef(I3MCHit,1);
@@ -82,4 +96,3 @@ class I3MCHit : public I3Hit
 typedef shared_ptr<I3MCHit>  I3MCHitPtr;
 
 #endif
-
