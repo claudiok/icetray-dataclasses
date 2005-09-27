a1047384e346696ce6d6d25762bddff8f3ef531c
Troy Straszheim <troy@icecube.umd.edu>
2005-09-27 09:46:14 +0000

    boost stuff moved here temporarily
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/branches/fast@11092 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/BoostHeaders.h
@@ -0,0 +1,66 @@
+#ifndef BOOSTHEADERS_H
+#define BOOSTHEADERS_H
+
+#include <boost/archive/polymorphic_oarchive.hpp>
+#include <boost/archive/polymorphic_iarchive.hpp>
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/is_abstract.hpp>
+#include <boost/serialization/export.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/serialization/split_member.hpp>
+
+using boost::serialization::make_nvp;
+using boost::serialization::base_object;
+
+template <typename T>
+struct instantiate_polymorphics
+{
+  typedef boost::archive::detail::interface_oarchive<
+    boost::archive::polymorphic_oarchive> po;
+
+  typedef boost::archive::detail::interface_iarchive<
+    boost::archive::polymorphic_iarchive> pi;
+
+  void instantiate(po & oa, const T & t);
+  void instantiate(pi & ia, T & t);
+};
+
+template <typename T>
+void instantiate_polymorphics<T>::instantiate(po & oa, const T & t) 
+{
+  oa & t;
+}
+
+template <typename T>
+void instantiate_polymorphics<T>::instantiate(pi & ia, T & t) 
+{
+  ia & t;
+}
+
+namespace detail 
+{
+  template <typename T>
+  void inst (boost::archive::polymorphic_oarchive& poa, const T* t)
+  {
+    poa & t;
+  }
+
+  template <typename T>
+  void inst (boost::archive::polymorphic_iarchive& pia, T* t)
+  {
+    pia & t;
+  }
+}
+
+#define I3_SERIALIZABLE(T)						\
+  template void detail::inst(boost::archive::polymorphic_oarchive&, const T*); \
+  template void detail::inst(boost::archive::polymorphic_iarchive&, T*); \
+  BOOST_SHARED_POINTER_EXPORT(T);					
+
+#endif
