24647bc5ddf57f224923d78ae4cfdece755cba59
Tom McCauley <tpmccauley@lbl.gov>
2005-10-11 01:34:46 +0000

    updated the test
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@11347 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3CalibrationTest.cxx
+++ private/test/I3CalibrationTest.cxx
@@ -14,89 +14,92 @@ using std::endl;
 
 TEST_GROUP(I3CalibrationTest);
 
-// Test file I/O
-TEST(file_io)
+// Test conversion back and forth from counts to volts
+TEST(conversion_counts_to_volts)
 {	
-  try
+    try
     {
-      I3Calibration* calib = new I3Calibration();
-      //	    calib->ls();
+	I3Calibration* calib = new I3Calibration();
       
-      I3InIceCalibration& inice_calib = calib->GetInIceCalibration();
-      //	    inice_calib.ls();
+	I3InIceCalibration& inice_calib = calib->GetInIceCalibration();
       
-      I3DOMCalibPtr dom_calib(new I3DOMCalibration);
-      //	    dom_calib->ls();
+	I3DOMCalibPtr dom_calib(new I3DOMCalibration);
       
-      int id = 0;
-      int channel = 0;
-      int bin = 127;
+	int id = 0;
+	int channel = 0;
+	int bin = 127;
       
-      double slope = -0.002;
-      double intercept = 2.9; 
-      double regress_coeff = 0.99;
-      double gain = -17.0;
+	// NOTE: In next iteration test the units
+	double slope = -0.002;  // volt/count
+	double intercept = 2.9; // volt
+	double regress_coeff = 0.99; 
+	double gain = -17.0;
+	double gainErr = 0.0;
+
+	double fe_pedestal = 2.6; // volt
+	int count = 100;
+
+	dom_calib->SetATWDGain(channel,gain,gainErr);
+	dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
       
-      double val;
-      double gainErr = 0.0;
+	inice_calib[OMKey(20,20)] = dom_calib;
       
-      val = slope;
+	I3DOMCalibPtr domptr = inice_calib[OMKey(20,20)];
       
-      dom_calib->SetATWDGain(channel,gain,gainErr);
-      dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
+	ENSURE_DISTANCE(gain, domptr->GetATWDGain(0), 0.0001,
+			"Failed to get gain from "
+			"I3DOMCalibration ptr");
       
-      inice_calib[OMKey(20,20)] = dom_calib;
-      
-      I3DOMCalibPtr domptr = inice_calib[OMKey(20,20)];
-      
-      ENSURE_DISTANCE(gain, domptr->GetATWDGain(0), 0.0001,
-		      "Failed to get gain from I3DOMCalibration "
-		      "read from disk");
-      ENSURE_DISTANCE(intercept, 
-		      domptr->GetATWDVoltage(0, 0,0,0.0, 0)*gain, 
-		      0.0001,
-		      "Failed to get voltage from "
-		      "I3DOMCalibration read from disk");
+	ENSURE_DISTANCE(intercept, 
+			domptr->GetATWDVoltage(0,0,0,0.0,0)*gain, 
+			0.0001,
+			"Failed to get voltage from "
+			"I3DOMCalibration ptr");
+
+	
+	double voltage = domptr->GetATWDVoltage(0,0,0, fe_pedestal, count);
+
+	ENSURE_DISTANCE(count,
+			domptr->GetATWDCount(0,0,0, fe_pedestal, voltage),
+			0.0001,
+			"Failed to convert count to voltage");
     }
   
-  catch(exception& e)
+    catch(exception& e)
     {
-      ENSURE("No exceptions thrown ",0);
+	ENSURE("No exceptions thrown ",0);
     }
 }
 
   
-  // Test I/O streams
+// Test I/O streams
 TEST(to_stream)
 {	
-  try
+    try
     {
-      I3DOMCalibPtr dom_calib(new I3DOMCalibration);
+	I3DOMCalibPtr dom_calib(new I3DOMCalibration);
       
-      int id = 0;
-      int channel = 0;
-      int bin = 128;
+	int id = 0;
+	int channel = 0;
+	int bin = 128;
       
-      double slope = -0.002;
-      double intercept = 2.9; 
-      double regress_coeff = 0.99;
-      double gain = -17.0;
+	double slope = -0.002;
+	double intercept = 2.9; 
+	double regress_coeff = 0.99;
+	double gain = -17.0;
       
-      double val;
-      double gainErr = 0.0;
+	double gainErr = 0.0;
       
-      val = slope;
+	dom_calib->SetATWDGain(channel,gain,gainErr);
+	dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
       
-      dom_calib->SetATWDGain(channel,gain,gainErr);
-      dom_calib->SetATWDParameters(id,channel,bin,slope,intercept,regress_coeff);
-      
-      dom_calib->ToStream(cout);
-      cout<< dom_calib;
+	dom_calib->ToStream(cout);
+	cout<< dom_calib;
     }
   
-  catch(exception& e)
+    catch(exception& e)
     {
-      ENSURE("No exceptions thrown",0);
+	ENSURE("No exceptions thrown",0);
     }
 }
 
