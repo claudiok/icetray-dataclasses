7dd90020c39437801a93601c1752d0a60c50c08f
Douglas Rutledge <rutledge@susqu.edu>
2005-12-11 19:05:59 +0000

    Committing my change to the I3FlasherInfo class. DLR 11Dec2005
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@13483 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3FlasherInfo.cxx
+++ private/dataclasses/I3FlasherInfo.cxx
@@ -1,23 +1,20 @@
 #include <dataclasses/BoostHeaders.h>
 #include <dataclasses/I3FlasherInfo.h>
-#include <dataclasses/I3Trigger.h>
-
-I3FlasherInfo::~I3FlasherInfo() { }
 
 template <class Archive>
     void I3FlasherInfo::serialize(Archive& ar, unsigned version)
 	{
-	    ar & make_nvp("I3Trigger", base_object<I3Trigger>(*this) );
+	    ar & make_nvp("I3Trigger", base_object<I3Trigger>(*this) );	    
 	    ar & make_nvp("FlasherOM", flasherOM_);
 	    ar & make_nvp("FlashTime", flashTime_);
 	    ar & make_nvp("ATWDBinSize", aTWDBinSize_);
 	    //ar & make_nvp("Trigger", trigger_);
 	    //trigger_ isn't defined anywhere
 	    ar & make_nvp("RawATWD3", rawATWD3_);
-	    // tpm ar & make_nvp("PedestalSubtractedATWD3", pedestalSubtractedATWD3_);
+	    ar & make_nvp("PedestalSubtractedATWD3", pedestalSubtractedATWD3_);
 	    ar & make_nvp("mask", mask_);
 	    ar & make_nvp("LEDBrightness", LEDBrightness_);
-	    ar & make_nvp("muon", muon_);
+	    ar & make_nvp("EventFlasherStatus", eventFlasherStatus_);
 	}
 
 
