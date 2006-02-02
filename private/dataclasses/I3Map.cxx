c7cd5d1df2919b17e7e1c879cbfe2218ac2c8311
A Philip Roth <aproth@icecube.umd.edu>
2006-02-02 18:51:16 +0000

    Trying to make I3Map<OMKey,I3RecoPulseSeries> serializable.  It still won't work in the data_shovel yet.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/branches/dc2@15219 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3Map.cxx
+++ private/dataclasses/I3Map.cxx
@@ -3,9 +3,13 @@
 #include <dataclasses/OMKey.h>
 #include <dataclasses/physics/I3DOMLaunchSeries.h>
 #include <dataclasses/physics/I3RecoHitSeries.h>
+#include <dataclasses/physics/I3RecoPulseSeries.h>
 
 typedef I3Map<OMKey, I3DOMLaunchSeries> RawData;
 I3_SERIALIZABLE(RawData);
 
 typedef I3Map<OMKey, I3RecoHitSeries> RecoHits;
 I3_SERIALIZABLE(RecoHits);
+
+typedef I3Map<OMKey, I3RecoPulseSeries> RecoPulses;
+I3_SERIALIZABLE(RecoPulses);
