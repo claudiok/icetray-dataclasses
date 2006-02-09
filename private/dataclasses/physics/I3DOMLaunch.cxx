7f2ae82ffe52bb5029df4bceeae06fb8a12fdff4
Torsten Schmidt <hightech-consulting@gmx.de>
2006-02-09 23:37:00 +0000

    I3DOMLaunch has a reduced set of member variables now - no calibration data is included anymore.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/branches/dc2@15514 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/physics/I3DOMLaunch.cxx
+++ private/dataclasses/physics/I3DOMLaunch.cxx
@@ -4,28 +4,27 @@
 I3DOMLaunch::~I3DOMLaunch() {}
 
 I3DOMLaunch::I3DOMLaunch() 
-    : startTime_(0.0)
-{}
+    : startTime_(0.0),
+      trigger_(0),
+      whichATWD_(ATWDa),
+      localCoincidence_(false),
+      chargeStampRange_(false)
+{
+  rawATWD_.resize(4);
+}
 
 template <class Archive>
     void I3DOMLaunch::serialize(Archive& ar, unsigned version)
 	{
 	  ar & make_nvp("TObject", base_object< TObject >(*this));
 	    ar & make_nvp("StartTime", startTime_);
-	    ar & make_nvp("ATWDBinSize", aTWDBinSize_);
 	    ar & make_nvp("Trigger", trigger_);
 	    ar & make_nvp("WhichATWD", whichATWD_);
-	    ar & make_nvp("RawATWD0", rawATWD0_);
-	    ar & make_nvp("RawATWD1", rawATWD1_);
-	    ar & make_nvp("RawATWD2", rawATWD2_);
-	    ar & make_nvp("RawATWD3", rawATWD3_);
+	    ar & make_nvp("RawATWD", rawATWD_);
 	    ar & make_nvp("RawFADC", rawFADC_);
-	    ar & make_nvp("CalibratedATWD", calibratedATWD_);
-	    ar & make_nvp("CalibratedFADC", calibratedFADC_);
 	    ar & make_nvp("LocalCoincidence", localCoincidence_);
-	    ar & make_nvp("ChargeStampRange", chargeStampRange_);
 	    ar & make_nvp("RawChargeStamp", rawChargeStamp_);
-	    ar & make_nvp("CalibratedChargeStamp", calibratedChargeStamp_);
+	    ar & make_nvp("ChargeStampRange", chargeStampRange_);
 	}
 
 I3_SERIALIZABLE(I3DOMLaunch);
--- public/dataclasses/physics/I3DOMLaunch.h
+++ public/dataclasses/physics/I3DOMLaunch.h
@@ -10,9 +10,8 @@
 #define I3DOMLAUNCH_H_INCLUDED
 
 #include "TObject.h"
-#include "dataclasses/Utility.h"
+#include "dataclasses/I3Vector.h"
 
-using namespace std; 
 /**
  * @brief The direct (digital) readout of an IceCube DOM
  * 
@@ -23,14 +22,13 @@ using namespace std;
  * stored in the ATWDBinSize variable.
  *
  */
-
 class I3DOMLaunch : public TObject
 {
 
 public:  
     enum ATWDselect {ATWDa,ATWDb};
 
-protected:
+private:
     /**  
      * This is the time (in nsec) in 25 nsec units, of the DOM clock 
      * which launches the ATWD (launch is synchronized to the first clock 
@@ -44,52 +42,20 @@ protected:
      */
     int trigger_;
 
-    /** 
-     * This is the ATWD time bin size, in nsec 
-     */
-    double aTWDBinSize_;
-
     /**  
      * This tells which ATWD in the DOM was used
      */
     ATWDselect whichATWD_;
 
     /** 
-     * Raw ATWD channel 0 waveform
+     * Raw ATWD channel 0 to 3
      */
-    vector<int> rawATWD0_;
-
-    /** 
-     * Raw ATWD channel 1 waveform
-     */
-    vector<int> rawATWD1_;
-
-    /** 
-     * Raw ATWD channel 2 waveform 
-     */
-    vector<int> rawATWD2_;
-
-    /** 
-     * Raw ATWD channel 3 waveform
-     */
-    vector<int> rawATWD3_;
-
-    /**
-     * This holds the pedestal-subtracted and gain-corrected ATWD waveform.
-     * Depending on the specific calibration, this is either a combination
-     * of the ATWD channels or the lowest gain channel that doesn't have saturation.
-     */
-    vector<double> calibratedATWD_;
+    I3Vector<I3Vector<int> > rawATWD_;
 
     /** 
      * This holds the 40 MHz FADC data 
      */
-    vector<int> rawFADC_;
-
-    /**
-     * This holds the pedestal-subtracted and gain-corrected FADC data
-     */
-    vector<double> calibratedFADC_;
+    I3Vector<int> rawFADC_;
 
     /** 
      * This holds the local coincidence bit
@@ -99,7 +65,7 @@ protected:
     /**
      * Raw course charge stamp
      */
-    vector<int> rawChargeStamp_;
+    I3Vector<int> rawChargeStamp_;
 
     /**
      * The raw course charge stamp is 9-bit, so
@@ -107,257 +73,118 @@ protected:
      * upper or lower 10-bit range 
      */
     bool chargeStampRange_;
-    
-    /**
-     * Calibrated course charge stamp
-     */
-    vector<double> calibratedChargeStamp_;
 
 public:
 
+    /**
+     * Default constructor.
+     */
     I3DOMLaunch();
 
+    /**
+     * Destructor.
+     */
     virtual ~I3DOMLaunch();
   
     /**
-     * return and set ATWD/FADC launch time
+     * Return ATWD/FADC launch time.
      */
     double GetStartTime() const { return startTime_; }
+
+    /**
+     * Set ATWD/FADC launch time.
+     */
     void SetStartTime(double starttime) { startTime_ = starttime; }
 
     /**
-     * return and set ATWD time bin size
+     * Return which ATWD.
      */
-    double GetATWDBinSize() const {return aTWDBinSize_;}
-    void SetATWDBinSize(double ATWDbinsize) {aTWDBinSize_ = ATWDbinsize;}
+    ATWDselect GetWhichATWD() const { return whichATWD_; }
 
     /**
-     * return and set WhichATWD
+     * Set which ATWD.
      */
-    ATWDselect GetWhichATWD() const {return whichATWD_;}
-    void SetWhichATWD(ATWDselect WhichATWD) {whichATWD_ = WhichATWD;}
+    void SetWhichATWD(ATWDselect WhichATWD) { whichATWD_ = WhichATWD; }
 
     /**
-     * return raw ATWD 0 waveform as a read-only object
+     * Return raw ATWD 0 waveform.
      */
-    const vector<int>& GetRawATWD0() const {return rawATWD0_;}
+    const I3Vector<int>& GetRawATWD0() const { return rawATWD_[0]; }
+    I3Vector<int>& GetRawATWD0() { return rawATWD_[0]; }
   
     /**
-     * return raw ATWD 1 waveform as a read-only object
+     * Return raw ATWD 1 waveform.
      */
-    const vector<int>& GetRawATWD1() const {return rawATWD1_;}
+    const I3Vector<int>& GetRawATWD1() const { return rawATWD_[1]; }
+    I3Vector<int>& GetRawATWD1() { return rawATWD_[1]; }
 
     /**
-     * return raw ATWD 2 waveform as a read-only object
+     * Return raw ATWD 2 waveform.
      */
-    const vector<int>& GetRawATWD2() const {return rawATWD2_;}
+    const I3Vector<int>& GetRawATWD2() const { return rawATWD_[2]; }
+    I3Vector<int>& GetRawATWD2() { return rawATWD_[2]; }
 
     /**
-     * return raw ATWD 3 waveform as a read-only object
+     * Return raw ATWD 3 waveform.
      */
-    const vector<int>& GetRawATWD3() const {return rawATWD3_;}
+    const I3Vector<int>& GetRawATWD3() const { return rawATWD_[3]; }
+    I3Vector<int>& GetRawATWD3() { return rawATWD_[3]; }
 
     /**
-     * return raw ATWD by channel number
+     * Return raw ATWD by channel number
      */
-    const vector<int>& GetRawATWD(int channel) const
-	{
-	    if ( channel == 0 )
-	    {
-		return rawATWD0_;
-	    }
-	    
-	    else if ( channel == 1 )
-	    {
-		return rawATWD1_;
-	    }
-	    
-	    else if ( channel == 2 )
-	    {
-		return rawATWD2_;
-	    }
-	    
-	    else if ( channel == 3 )
-	    {
-		return rawATWD3_;
-	    }
-	    
-	    else
-	    {
-		log_fatal("Bad ATWD channel in I3DOMLaunch::GetRawATWD(channel)");
-                return *(vector<int>*)0;
-	    }
-	}
+    const I3Vector<int>& GetRawATWD(unsigned int channel) const
+    {
+      if(channel >= rawATWD_.size())
+        log_fatal("Bad ATWD channel in I3DOMLaunch::GetRawATWD(channel)");
+      return rawATWD_[channel];
+    }
+
+    I3Vector<int>& GetRawATWD(unsigned int channel)
+    {
+      if(channel >= rawATWD_.size())
+        log_fatal("Bad ATWD channel in I3DOMLaunch::GetRawATWD(channel)");
+      return rawATWD_[channel];
+    }
         
     /**
-     * return raw FADC waveform as a read-only object
+     * Return raw FADC waveform.
      */
-    const vector<int>& GetRawFADC() const {return rawFADC_;}
+    const I3Vector<int>& GetRawFADC() const { return rawFADC_; }
+    I3Vector<int>& GetRawFADC() { return rawFADC_; }
 
     /**
-     * return the pedestal subtracted and gain-corrected (i.e. calibrated) FADC
+     * Return local coincidence bit.
      */
-    const vector<double>& GetCalibratedFADC() const 
-	{
-	    return calibratedFADC_;
-	}
-  
-    /** 
-     * return calibrated ATWD waveform
-     */
-    const vector<double>& GetCalibratedATWD() const
-	{
-	    return calibratedATWD_;
-	}
-
-    /**
-     * return local coincidence bit as a read-only object
-     */
-    bool GetLCBit() const {return localCoincidence_;}
-
-    /** 
-     * Return the raw charge stamp
-     */
-    const vector<int>& GetRawChargeStamp() const 
-	{
-	    return rawChargeStamp_;
-	};
-
-    /**
-     * Return the range for the raw charge stamp
-     */
-    const bool GetChargeStampRange() const
-	{
-	    return chargeStampRange_;
-	};
-    
-    /**
-     * Return the calibrated charge stamp
-     */
-    const vector<double>& GetCalibratedChargeStamp() const
-	{
-	    return calibratedChargeStamp_;
-	};
-   
-    /**
-     * sets the raw ATWD0 waveform
-     */
-    void SetRawATWD0(const vector<int>& ATWD0data) {rawATWD0_=ATWD0data;}
-
-    /**
-     * sets the raw ATWD1 waveform
-     */
-    void SetRawATWD1(const vector<int>& ATWD1data) {rawATWD1_=ATWD1data;}
-
-    /**
-     * sets the raw ATWD2 waveform
-     */
-    void SetRawATWD2(const vector<int>& ATWD2data) {rawATWD2_=ATWD2data;}
-
-    /**
-     * sets the ATWD2 waveform
-     */
-    void SetRawATWD3(const vector<int>& ATWD3data) {rawATWD3_=ATWD3data;}
-
-    /** 
-     * set ATWD by channel number
-     */
-    void SetRawATWD(int channel, const vector<int>& ATWDdata)
-	{
-	    if ( channel == 0 )
-	    {
-		rawATWD0_ = ATWDdata;
-		return;
-	    }
-	    
-	    else if ( channel == 1 )
-	    {
-		rawATWD1_ = ATWDdata;
-		return;
-	    }
-	    
-	    else if ( channel == 2 )
-	    {
-		rawATWD2_ = ATWDdata;
-		return;
-	    }
-	    
-	    else if ( channel == 3 )
-	    {
-		rawATWD3_ = ATWDdata;
-		return;
-	    }
-	    
-	    else 
-	    {
-		log_fatal("Bad ATWD channel in I3DOMLaunch::GetRawATWD(channel)");
-		throw std::exception();
-	    }
-	}    
-
-    /**
-     * sets the raw FADC waveform
-     */
-    void SetRawFADC(const vector<int>& FADCdata) {rawFADC_=FADCdata;}
-
-    /**
-     * sets the calibrated FADC waveform
-     */
-    void SetCalibratedFADC (const vector<double> &pgfadc_data) { calibratedFADC_ = pgfadc_data;}
-
-    /**
-     * sets the combined ATWD waveform
-     */
-    void SetCalibratedATWD(const vector<double>& CalibratedATWD) 
-	{
-	    calibratedATWD_ = CalibratedATWD;
-	};
+    bool GetLCBit() const { return localCoincidence_; }
   
     /**
-     * sets the local coincidence bit
+     * Set the local coincidence bit.
      */
-    void SetLCBit(const bool & LCBit) {localCoincidence_=LCBit;}
-
-    /**
-     * Set the coarse charge stamp
-     */
-    void SetRawChargeStamp(const vector<int>& chargeStamp)
-	{
-	    rawChargeStamp_ = chargeStamp;
-	};
+    void SetLCBit(bool LCBit) {localCoincidence_=LCBit;}
 
     /** 
-     * Set the bit range for the raw charge stamp
+     * Return the raw charge stamp.
      */
-    void SetChargeStampRange(const bool range)
-	{
-	    chargeStampRange_ = range;
-	};
-    
-    /** 
-     * Set the calibrated charge stamp
-     */
-    void SetCalibratedChargeStamp(const vector<double>& chargeStamp)
-	{
-	    calibratedChargeStamp_ = chargeStamp;
-	};
-    
+    const I3Vector<int>& GetRawChargeStamp() const { return rawChargeStamp_; }
+    I3Vector<int>& GetRawChargeStamp() { return rawChargeStamp_; }
+
     /**
-     * Dumps the file to the given ostream
+     * Return the range for the raw charge stamp.
      */
-    virtual void ToStream(ostream& o) const
-	{
-	    o<<"StartTime: "<< startTime_;
-	}
+    const bool GetChargeStampRange() const { return chargeStampRange_; }
+
+    /** 
+     * Set the range for the raw charge stamp.
+     */
+    void SetChargeStampRange(bool range)
+    {
+      chargeStampRange_ = range;
+    };
 
 private:
-
     friend class boost::serialization::access;
-
     template <class Archive> void serialize(Archive & ar, unsigned version);
-    
-    // ROOT macro
-    //ClassDef(I3DOMLaunch,1);
 };
 
 I3_POINTER_TYPEDEFS(I3DOMLaunch);
