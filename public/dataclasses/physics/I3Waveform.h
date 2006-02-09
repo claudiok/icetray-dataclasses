620505883d621a751355b4ded695c7efbd79ab60
John Pretz <pretz@icecube.umd.edu>
2006-02-09 16:55:56 +0000

    Waveform class flushed out.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/branches/dc2@15504 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/physics/I3Waveform.h
+++ public/dataclasses/physics/I3Waveform.h
@@ -2,22 +2,46 @@
 #define I3_WAVEFORM_H
 
 #include <vector>
+#include <TObject.h>
 
 using namespace std;
 
-class I3Waveform
+class I3Waveform : public TObject
 {
-#warning implement me
+ public:
+  enum Source
+    {
+      ATWD,
+      FADC,
+      TWR_ELECTRICAL,
+      TWR_OPTICAL,
+      ETC
+    };
+
+ private:
   double startTime_;
   double binWidth_;
   vector<double> waveform_;
-  enum Source
-    {
-      TWR,
-      ATWD,
-      FADC,
-      ETC
-    };
+  Source source_;
+  
+  double GetStartTime() const {return startTime_;}
+
+  void SetStartTime(double startTime){startTime_ = startTime;}
+
+  double GetBinWidth() const {return binWidth_;}
+
+  void SetBinWidth(double binWidth){binWidth_ = binWidth;}
+
+  const vector<double>& GetWaveform() const {return waveform_;}
+
+  void SetWaveform(const vector<double>& waveform){waveform_ = waveform;}
+
+  Source GetSource() const{return source_;}
+
+  void SetSource(Source source){source_ = source;}
+
+  template<class Archive>
+    void serialize(Archive& ar, unsigned version);
 };
 
 #endif
