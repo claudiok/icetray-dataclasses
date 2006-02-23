d11f61e8b37393aad36a8fa0d0e2e690520feb84
Troy Straszheim <troy@icecube.umd.edu>
2006-02-23 21:39:09 +0000

    a couple explicit instantiations for good measure.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@16224 16731396-06f5-0310-8873-f7f720988828

---
+++ private/dataclasses/I3Map.cxx
@@ -0,0 +1,13 @@
+
+#include <dataclasses/BoostHeaders.h>
+#include <dataclasses/I3Map.h>
+#include <string>
+
+typedef I3Map<std::string, double> I3MapStringDouble;
+typedef I3Map<std::string, int> I3MapStringInt;
+
+
+I3_SERIALIZABLE(I3MapStringDouble);
+
+I3_SERIALIZABLE(I3MapStringInt);
+
