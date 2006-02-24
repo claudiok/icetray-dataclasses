563945a082008787fbf6ee7f9cad800a58295022
Brian Christy <bchristy@umdgrb.umd.edu>
2006-02-24 16:33:44 +0000

    Revision to dis-allow automatic typeconversion with Double.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@16262 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3Double.cxx
+++ private/dataclasses/I3Double.cxx
@@ -1,13 +1,28 @@
-
 #include <dataclasses/BoostHeaders.h>
 #include <dataclasses/I3Double.h>
+
+I3Double::I3Double() : value(0) {}
+
+I3Double::I3Double(double d) : value(d) {}
+
+I3Double::I3Double(const I3Double& rhs) : value(rhs.value) {}
+
+I3Double&
+I3Double::operator=(const I3Double& rhs)
+{
+  //check for self-assignment
+  if (this != &rhs)
+    value = rhs.value;
+
+  return *this;
+}
 
 template <class Archive>
 void
 I3Double::serialize(Archive& ar,unsigned)
 {
   ar & make_nvp("TObject",base_object<TObject>(*this));
-  ar & make_nvp("value",value_);
+  ar & make_nvp("value",value);
 }
 
 I3_SERIALIZABLE(I3Double);
