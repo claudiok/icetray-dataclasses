19b0eca4a653546350103c62b038588ce438544a
Troy Straszheim <troy@icecube.umd.edu>
2006-02-27 19:21:45 +0000

    Test these insofar as it makes sense to test them.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@16452 16731396-06f5-0310-8873-f7f720988828

---
+++ private/test/AsXMLTest.cxx
@@ -0,0 +1,38 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: ToStringTest.cxx 9329 2005-06-20 22:36:05Z dule $
+*/
+
+#include <I3Test.h>
+
+#include "dataclasses/Utility.h"
+#include "dataclasses/physics/I3Particle.h"
+
+using namespace std;
+
+TEST_GROUP(AsXMLTest);
+
+TEST(I3Position)
+{
+  I3Position pos;
+  cout << AsXML(pos);
+  I3PositionPtr pptr(new I3Position);
+  cout << AsXML(pptr);
+}
+
+TEST(I3Direction)
+{
+  I3Direction dir;
+  cout << AsXML(dir);
+  I3DirectonPtr dirptr(new I3Direction);
+  cout << AsXML(dirptr);
+  I3DirectionConstPtr dircnstptr(new I3Direction);
+  cout << AsXML(dircnstptr);
+}
+
+TEST(I3Particle)
+{
+  I3Particle muon;
+  cout << AsXML(muon);
+}
