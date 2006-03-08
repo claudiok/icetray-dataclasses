7c8d1f561bf0e2b51c9d6b37383f1b751769dab4
Troy Straszheim <troy@icecube.umd.edu>
2006-03-08 14:26:36 +0000

    Such a sweet little toplevel string container class.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@16899 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3String.h
@@ -0,0 +1,26 @@
+#ifndef DATACLASSES_I3STRING_H_INCLUDED
+#define DATACLASSES_I3STRING_H_INCLUDED
+
+#include <icetray/I3FrameObject.h>
+#include <string>
+
+// A holder for a string if you want to put one in the frame.  *Dont*
+// use these if you're not putting this thing in the frame!  That's
+// messy!
+
+struct I3String : public I3FrameObject
+{
+  std::string value;
+  
+  I3String();
+  I3String(string s);
+  I3String(const I3String&);
+  I3String& operator=(const I3String&);
+
+  template <typename Archive>
+    void serialize(Archive& ar, unsigned version);
+};
+
+I3_POINTER_TYPEDEFS(I3String);
+
+#endif
