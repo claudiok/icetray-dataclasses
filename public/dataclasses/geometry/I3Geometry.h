ddd6771a0b96b53a7689a02efcc89bb7d42c9c03
Alex Olivas <olivas@icecube.umd.edu>
2006-03-21 17:38:46 +0000

    copy constructor can be useful for tests.  making a geometry on-the-fly and passing it to a method that expects a const from the frame.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@17600 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/geometry/I3Geometry.h
+++ public/dataclasses/geometry/I3Geometry.h
@@ -67,7 +67,6 @@ I3_POINTER_TYPEDEFS(I3StationGeoMap)
   I3Time startTime;
   I3Time endTime;
 
-  I3Geometry(const I3Geometry& geometry);
   const I3Geometry& operator=(const I3Geometry& geometry);
 
   friend class boost::serialization::access;
