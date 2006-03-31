616532cafcb7e242b1974d4e74eab5dacce6bb19
Katherine Rawlins <kath@uaa.alaska.edu>
2006-03-31 22:06:03 +0000

    Added I3Vector<OMKey> to serialization registration.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@18151 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3Vector.h
+++ public/dataclasses/I3Vector.h
@@ -11,6 +11,7 @@
 #include "icetray/I3FrameObject.h"
 #include <vector>
 #include "dataclasses/Utility.h"
+#include "dataclasses/OMKey.h"
 
 template <typename T>
 struct I3Vector : public I3FrameObject, public std::vector<T>
@@ -27,11 +28,13 @@ typedef I3Vector<string> I3VectorString;
 typedef I3Vector<double> I3VectorDouble;
 typedef I3Vector<int> I3VectorInt;
 typedef I3Vector<bool> I3VectorBool;
+typedef I3Vector<OMKey> I3VectorOMKey;
 
 I3_POINTER_TYPEDEFS(I3VectorString);
 I3_POINTER_TYPEDEFS(I3VectorDouble);
 I3_POINTER_TYPEDEFS(I3VectorInt);
 I3_POINTER_TYPEDEFS(I3VectorBool);
+I3_POINTER_TYPEDEFS(I3VectorOMKey);
 
 #endif // I3VECTOR_H_INCLUDED
 
