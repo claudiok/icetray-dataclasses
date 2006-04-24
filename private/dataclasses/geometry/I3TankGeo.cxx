97bbf15de9283e729456a8dd4d54580862bbb0ec
Peter Niessen <niessen@ifh.de>
2006-04-24 21:57:00 +0000

    Added list of OMs for each tank, increased version. Maybe not necessary to write the list into the output file, though.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/branches/v2-beta-3@18915 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/geometry/I3TankGeo.cxx
+++ private/dataclasses/geometry/I3TankGeo.cxx
@@ -6,14 +6,15 @@ I3TankGeo::~I3TankGeo() {}
 template <class Archive>
   void I3TankGeo::serialize(Archive& ar, unsigned version)
   {
+
     ar & make_nvp("Position", position);
     ar & make_nvp("Orientation", orientation);
     ar & make_nvp("TankRadius", tankradius);
     ar & make_nvp("TankHeight", tankheight);
     ar & make_nvp("FillHeight", fillheight);
+    ar & make_nvp("OMKeyList", omKeyList_);
 
   }
 
-
-
+BOOST_CLASS_VERSION(I3TankGeo, 1);
 I3_SERIALIZABLE(I3TankGeo);
