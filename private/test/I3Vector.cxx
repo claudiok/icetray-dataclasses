b1e42a28216f350b1c59dffe28f18aac9ffdba4f
Erik Blaufuss <blaufuss@icecube.umd.edu>
2006-05-24 17:04:30 +0000

    Remove warns, imported from release
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@20054 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3Vector.cxx
+++ private/test/I3Vector.cxx
@@ -29,22 +29,24 @@ TEST(i3vector_to_set_and_back)
   keyvec.push_back(OMKey(1,1));
   keyvec.push_back(OMKey(2,2));
   keyvec.push_back(OMKey(1,1));
+  unsigned int expected_size = 6;
+  unsigned int expected_size2 = 3;
 
-  ENSURE_EQUAL(keyvec.size(), 6);
+  ENSURE_EQUAL(keyvec.size(), expected_size);
 
   // make a std::set
 
   set<OMKey> keyset(keyvec.begin(), keyvec.end());
 
-  ENSURE_EQUAL(keyset.size(), 3);
+  ENSURE_EQUAL(keyset.size(), expected_size2);
 
   // make a std::vector from the std::set
   vector<OMKey> keyvec_2(keyset.begin(), keyset.end());
-  ENSURE_EQUAL(keyvec_2.size(), 3);
+  ENSURE_EQUAL(keyvec_2.size(), expected_size2);
 
   // make an I3Vector from the std::set
   I3Vector<OMKey> keyvec_3(keyset.begin(), keyset.end());
-  ENSURE_EQUAL(keyvec_3.size(), 3);
+  ENSURE_EQUAL(keyvec_3.size(), expected_size2);
 
 
 
