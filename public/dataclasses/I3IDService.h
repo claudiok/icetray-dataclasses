205af99a82d1c0551090208acd1c764b0c3e1bcf
Alex Olivas <olivas@icecube.umd.edu>
2006-06-15 14:07:16 +0000

    forgot to add these two files
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/branches/gp-prototype@20858 16731396-06f5-0310-8873-f7f720988828

---
+++ public/dataclasses/I3IDService.h
@@ -0,0 +1,52 @@
+/**
+ * copyright  (C) 2004
+ * the icecube collaboration
+ * $Id: I3IDService.h 17239 2006-03-14 17:51:36Z troy $
+ *
+ * @file I3IDService.h
+ * @version $Revision:$
+ * @date $Date: 2006-03-14 11:51:36 -0600 (Tue, 14 Mar 2006) $
+ * @author pretz
+ */
+
+#ifndef I3IDSERVICE_H
+#define I3IDSERVICE_H
+
+#include <icetray/I3FrameObject.h>
+#include <icetray/I3DefaultName.h>
+
+#include "dataclasses/Utility.h"
+
+class I3Frame;
+
+/**
+ * @brief This services simply serves up integers
+ * to guarantee all of the particle ids in an event 
+ * are unique
+ */
+class I3IDService
+{
+
+ private:
+  int id_;
+
+ public:
+
+  /**
+   * @brief This method should fill the frame with all the relevent
+   * frame data.  Returned is the time of the event.
+   */
+  int GetID(){ return id_++; }
+    
+
+  I3IDService():
+    id_(0)
+    {};
+
+  ~I3IDService() { }
+};
+
+I3_DEFAULT_NAME(I3IDService);
+I3_POINTER_TYPEDEFS(I3IDService);
+
+#endif
