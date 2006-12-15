c2921b851e2d34e74005fdc8e21ded5831e2b760
Erik Blaufuss <blaufuss@icecube.umd.edu>
2006-12-15 17:56:26 +0000

    More tests.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@26514 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3DirectionTest.cxx
+++ private/test/I3DirectionTest.cxx
@@ -106,11 +106,38 @@ TEST(coord_change)
   ENSURE_DISTANCE(dir.GetY(),0.57735,0.0001,"dir.GetY failed");
   ENSURE_DISTANCE(dir.GetZ(),0.57735,0.0001,"dir.GetZ failed");
   
+  cout <<"Creating another I3Direction..."<<endl;
+  I3Direction dir2;
+  dir2.SetDirection(1.0,0.0,1.0);  // Not properly normalized
+  //Recall Azimuth/Zenith are the direction back to sourcethat originates a particle
+  //  with this direction.
+  ENSURE_DISTANCE(dir2.GetZenith()/deg,(180.0-45.0),0.001,"dir.GetZenith failed");
+  ENSURE_DISTANCE(dir2.GetAzimuth()/deg,180.0,0.001,"dir.GetAzimuth failed");
+  ENSURE_DISTANCE(dir2.CalcTheta()/deg,(45.0),0.001,"dir.CalcTheta failed");
+  ENSURE_DISTANCE(dir2.CalcPhi()/deg,0.0,0.001,"dir.CalcPhi failed");
+  //Make sure the GetX, etc are properly renormalized.
+  ENSURE_DISTANCE(dir2.GetX(),0.7071,0.0001,"dir.GetX failed");
+  ENSURE_DISTANCE(dir2.GetY(),0.,0.0001,"dir.GetY failed");
+  ENSURE_DISTANCE(dir2.GetZ(),0.7071,0.0001,"dir.GetZ failed");
+  //Now change the direction, make sure everthing is updated properly
+  dir2.SetDirection(1.0,1.0,1.0);  // Not properly normalized
+  ENSURE_DISTANCE(dir2.GetZenith()/deg,(180.0-54.736),0.001,"dir.GetZenith failed");
+  ENSURE_DISTANCE(dir2.GetAzimuth()/deg,(225.0),0.001,"dir.GetAzimuth failed");
+  ENSURE_DISTANCE(dir2.CalcTheta()/deg,(54.736),0.001,"dir.CalcTheta failed");
+  ENSURE_DISTANCE(dir2.CalcPhi()/deg,45.0,0.001,"dir.GetPhi failed");
+  //Make sure the GetX, etc are properly renormalized.
+  ENSURE_DISTANCE(dir2.GetX(),0.57735,0.0001,"dir.GetX failed");
+  ENSURE_DISTANCE(dir2.GetY(),0.57735,0.0001,"dir.GetY failed");
+  ENSURE_DISTANCE(dir2.GetZ(),0.57735,0.0001,"dir.GetZ failed");
+
   cout <<"Setting dir: x=1,y=1,z=0..."<<endl;
   dir.SetDirection(1,1,0);
   ENSURE_DISTANCE(dir.GetZenith()/deg,90.0,0.001,"dir.GetZenith failed");
   ENSURE_DISTANCE(dir.GetAzimuth()/deg,225.0,0.001,"dir.GetAzimuth failed");
-  
+  ENSURE_DISTANCE(dir.GetX(),0.7071,0.001,"dir.X failed");
+  ENSURE_DISTANCE(dir.GetY(),0.7071,0.001,"dir.Y failed");
+  ENSURE_DISTANCE(dir.GetZ(),0.0,0.001,"dir.Z failed");
+
   cout <<"Setting values for dir in zen=0,azi=0 (i.e. down)..."<<endl;
   dir.SetDirection(0,0);
   ENSURE_DISTANCE(dir.GetX(),0.0,0.0001,"dir.GetX failed");
