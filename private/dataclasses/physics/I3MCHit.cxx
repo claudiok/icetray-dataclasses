196413dd6999bf3d3c3665a883b10c115e1da7a5
Alex Olivas <olivas@icecube.umd.edu>
2007-01-25 15:29:24 +0000

    just following orders and using icetray/serialization.h instead
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@28070 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/physics/I3MCHit.cxx
+++ private/dataclasses/physics/I3MCHit.cxx
@@ -1,4 +1,4 @@
-#include <dataclasses/BoostHeaders.h>
+#include <icetray/serialization.h>
 #include <dataclasses/physics/I3MCHit.h>
 #include <dataclasses/physics/I3Particle.h>
 I3MCHit::~I3MCHit() { }
