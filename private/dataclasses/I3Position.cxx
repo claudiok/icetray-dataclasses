63ddcd5d9a4ba7ec8437c1216d19c18b62de663d
Troy Straszheim <troy@icecube.umd.edu>
2007-05-29 20:35:49 +0000

    unsigned are %u, not %s.  Dont ignore warnings.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@32711 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3Position.cxx
+++ private/dataclasses/I3Position.cxx
@@ -18,7 +18,7 @@ void
 I3Position::serialize(Archive& ar, unsigned version)
 {
   if (version>i3position_version_)
-    log_fatal("Attempting to read version %s from file but running version %s of I3Position class.",version,i3position_version_);
+    log_fatal("Attempting to read version %u from file but running version %u of I3Position class.",version,i3position_version_);
 
   ar & make_nvp("I3FrameObject", base_object<I3FrameObject>(*this));
   ar & make_nvp("X", x_);
