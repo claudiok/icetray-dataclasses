7cc973a33740fcfb97e3b930264994eebfd486da
Philip Roth <aproth@icecube.umd.edu>
2008-01-02 20:49:02 +0000

    The I3Bool in dataclasses now just refers you to the one in icetray.  This allows all the existing code to remain unchanged.  The original motivation of moving I3Bool is to allow interfaces to depend only on icetray.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@40272 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/I3Bool.h
+++ public/dataclasses/I3Bool.h
@@ -1,29 +1,6 @@
 #ifndef DATACLASSES_I3BOOL_H_INCLUDED
 #define DATACLASSES_I3BOOL_H_INCLUDED
 
-#include "icetray/I3FrameObject.h"
-#include <dataclasses/Utility.h>
-
-//
-//  Dumb-struct holders of PODS should *not* automatically convert
-//  to/from the underlying type lest people think that it is necessary
-//  to use, say, vector<I3Double> instead of just vector<double>.
-//  They are instead holders of values, with member "value".
-//
-struct I3Bool : public I3FrameObject
-{
-  bool value;
-
-  I3Bool();
-  I3Bool(bool b);
-  I3Bool(const I3Bool&);
-  I3Bool& operator=(const I3Bool&);
-
-  template <typename Archive>
-    void serialize(Archive& ar,unsigned version);
-
-};
-
-I3_POINTER_TYPEDEFS(I3Bool);
+#include "icetray/I3Bool.h"
 
 #endif
