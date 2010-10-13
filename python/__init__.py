2fbdfdd87c1733fe4a817c3eb2ff40b7eb479454
Fabian Kislat <fabian.kislat@desy.de>
2010-10-13 15:17:43 +0000

    Pull in dependencies of dataclasses  --> No more "RuntimeError: extension class wrapper for base class I3FrameObject has not been created yet"
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@67491 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -1,8 +1,13 @@
 from icecube.load_pybindings import load_pybindings
+import icecube.icetray # be nice and pull in our dependencies
+
 load_pybindings(__name__,__path__)
+
 
 # Add some aliases for naming consistency
 I3RecoPulseSeries = vector_I3RecoPulse
 I3RecoHitSeries = vector_I3RecoHit
 I3TWRLaunchSeries = Vector_I3TWRLaunch
 I3MCHitSeries = vector_I3MCHit
+
+del icecube
