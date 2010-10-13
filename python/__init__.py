bc2154bc312c9c6df55c05853f18ed958db1ea38
Fabian Kislat <fabian.kislat@desy.de>
2010-10-13 15:14:54 +0000

    Add aliases for vector pybindings so that these classes can be used in the same way as in C++. Old scripts will still work and vector_... and ...Series can be used interchangeably.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@67490 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -3,3 +3,6 @@ load_pybindings(__name__,__path__)
 
 # Add some aliases for naming consistency
 I3RecoPulseSeries = vector_I3RecoPulse
+I3RecoHitSeries = vector_I3RecoHit
+I3TWRLaunchSeries = Vector_I3TWRLaunch
+I3MCHitSeries = vector_I3MCHit
