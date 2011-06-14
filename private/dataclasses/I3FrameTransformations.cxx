a819100e293ac22cde99b1b84f4c017fe3fc91a9
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-06-14 19:39:10 +0000

    Also provide a mechanism to read hit series maps as pulse series maps. This is step #1 toward deprecation of I3RecoHit.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/releases/q-frame-preview-2@76618 16731396-06f5-0310-8873-f7f720988828

--- private/dataclasses/I3FrameTransformations.cxx
+++ private/dataclasses/I3FrameTransformations.cxx
@@ -10,10 +10,14 @@
  *  
  */
 
-#include "icetray/I3Frame.h"
-#include "dataclasses/I3MapOMKeyMask.h"
-#include "dataclasses/physics/I3RecoPulse.h"
-#include "dataclasses/payload/I3SuperDST.h"
+#include <icetray/I3Frame.h>
+#include <icetray/I3Units.h>
+#include <dataclasses/I3MapOMKeyMask.h>
+#include <dataclasses/physics/I3RecoHit.h>
+#include <dataclasses/physics/I3RecoPulse.h>
+#include <dataclasses/payload/I3SuperDST.h>
+
+static I3RecoPulseSeriesMapPtr HitsAsPulses(I3RecoHitSeriesMapConstPtr hits);
 
 /*
  * Only a little bit evil: Specialize the I3Frame::Get() to apply
@@ -44,6 +48,36 @@ I3Frame::Get(const std::string& name, bool quietly, void*, void*) const
 	
 	if (superdst)
 		return superdst->Unpack();
+
+	// Compatibility with old data
+	I3RecoHitSeriesMapConstPtr hits = 
+	    dynamic_pointer_cast<const I3RecoHitSeriesMap>(focp);
+
+	if (hits)
+		return HitsAsPulses(hits);
 	else
 		return pulses;
 }
+
+static I3RecoPulseSeriesMapPtr
+HitsAsPulses(I3RecoHitSeriesMapConstPtr hits)
+{
+	I3RecoPulseSeriesMapPtr pulses(new I3RecoPulseSeriesMap);
+
+	for (I3RecoHitSeriesMap::const_iterator i = hits->begin();
+	    i != hits->end(); i++) {
+		I3RecoPulseSeries &ps = (*pulses)[i->first];
+		
+		for (I3RecoHitSeries::const_iterator j = i->second.begin();
+		    j != i->second.end(); j++) {
+			I3RecoPulse pulse;
+			pulse.SetTime(j->GetTime());
+			pulse.SetCharge(1.0 /* PE */);
+			pulse.SetWidth(3.3 * I3Units::ns /* XXX: ATWD bin */);
+			ps.push_back(pulse);
+		}
+	}
+
+	return pulses;
+}
+
