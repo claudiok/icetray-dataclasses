1404c6f829649f6f378c1a033937926b1fc62dec
Alex Olivas <olivas@icecube.umd.edu>
2011-07-29 07:59:20 +0000

    not going to test classes with non-trivial constructors and/or containers.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@78398 16731396-06f5-0310-8873-f7f720988828

--- resources/test/test_disabling_of_python_dynamism.py
+++ resources/test/test_disabling_of_python_dynamism.py
@@ -6,5 +6,8 @@ from icecube.icetray import test_disabling_of_python_dynamism as test_freeze
 
 # the default is an empty list so this is really not needed, but
 # is here as a reminder that you can skip classes
-skip_these = [""]
+# not going to worry about these
+skip_these = ["I3SuperDST","I3RecoPulseSeriesMapMask",\
+              "map_indexing_suite_map_Subdetector_I3TriggerReadoutConfig_entry",
+              "PairDoubleDouble"]
 ENSURE( test_freeze.test_all_classes(dc, skip_these) , "not cool" )
