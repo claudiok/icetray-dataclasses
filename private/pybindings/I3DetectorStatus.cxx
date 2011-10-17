bfad732af32eb44f150896f209165c9161b42c64
Jakob van Santen <jvansanten@gmail.com>
2011-10-17 14:49:20 +0000

    Fix bindings for I3DOMStatus::statusATWDa/b, which violate the assumptions of snake_case().
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@80758 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/I3DetectorStatus.cxx
+++ private/pybindings/I3DetectorStatus.cxx
@@ -74,9 +74,12 @@ void register_I3DetectorStatus()
     scope outer = 
       class_<I3DOMStatus, boost::shared_ptr<I3DOMStatus> >("I3DOMStatus")
       .def(copy_suite<I3DOMStatus>())
-      #define DOMSTATUSPROPERTIES (pmtHV)(lcMode)(txMode)(lcWindowPre)(lcWindowPost)(lcSpan)(statusATWDa)(statusATWDb)(statusFADC)(pmtHV)(speThreshold)(fePedestal)(dacTriggerBias0)(dacTriggerBias1)(dacFADCRef)(deltaCompress)(domGainType)(cableType)(SLCActive)(mpeThreshold)
+      #define DOMSTATUSPROPERTIES (pmtHV)(lcMode)(txMode)(lcWindowPre)(lcWindowPost)(lcSpan)(statusFADC)(pmtHV)(speThreshold)(fePedestal)(dacTriggerBias0)(dacTriggerBias1)(dacFADCRef)(deltaCompress)(domGainType)(cableType)(SLCActive)(mpeThreshold)
       BOOST_PP_SEQ_FOR_EACH(WRAP_RW_RECASE, I3DOMStatus, DOMSTATUSPROPERTIES)
       #undef DOMSTATUSPROPERTIES
+      // If we used snake_case, these two would end up as status_atw_da
+      .def_readwrite("status_atwd_a", &I3DOMStatus::statusATWDa)
+      .def_readwrite("status_atwd_b", &I3DOMStatus::statusATWDb)
       .def( freeze() )
       ;
 
