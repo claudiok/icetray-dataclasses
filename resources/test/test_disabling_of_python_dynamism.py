05053692658c5c8352f796e044649db4f4628759
Jakob van Santen <jvansanten@gmail.com>
2012-01-15 21:18:10 +0000

    Fix dynamism test failure introduced in r83900.
    def(freeze()) is not enough; you also need a nullary constructor. Update comments to reflect the nature of the gotcha.
    
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@83903 16731396-06f5-0310-8873-f7f720988828

--- resources/test/test_disabling_of_python_dynamism.py
+++ resources/test/test_disabling_of_python_dynamism.py
@@ -4,10 +4,8 @@ from icecube import dataclasses as dc
 import inspect
 from icecube.icetray import test_disabling_of_python_dynamism as test_freeze
 
-# the default is an empty list so this is really not needed, but
-# is here as a reminder that you can skip classes
-# not going to worry about these
-skip_these = ["I3SuperDST","I3RecoPulseSeriesMapMask",\
+# test_all_classes() fails for classes that don't have a nullary constructor.
+skip_these = ["I3SuperDST","I3SuperDSTTriggerSeries","I3RecoPulseSeriesMapMask",\
               "I3RecoPulseSeriesMapUnion","DroopedSPETemplate",\
               "map_indexing_suite_map_Subdetector_I3TriggerReadoutConfig_entry",
               "PairDoubleDouble"]
