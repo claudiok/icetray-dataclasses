f9a16d9e28f6ae4b3c09fdcd072a5205b64ce640
Alex Olivas <olivas@icecube.umd.edu>
2012-03-28 23:14:28 +0000

    cleaned up the code and tests for the trigger recipes
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@86665 16731396-06f5-0310-8873-f7f720988828

--- resources/test/test_trigger_hierachy.py
+++ resources/test/test_trigger_hierachy.py
@@ -9,19 +9,31 @@ from icecube.icetray.I3Test import *
 
 th = dataclasses.I3TriggerHierarchy()
 
-
-
 sid = dataclasses.I3Trigger.IN_ICE
 tid = dataclasses.I3Trigger.SIMPLE_MULTIPLICITY
 cid = 1008
 tkey = dataclasses.TriggerKey(sid, tid, cid)
-print find_triggers(th,tkey)
 
-print n_triggers(th, sourceID = sid)
-print n_triggers(th, typeID = tid)
-print n_triggers(th, configID = cid)
+tr = dataclasses.I3Trigger()
+tr.key = tkey
 
-print trigger_lengths(th, sourceID = sid)
-print trigger_lengths(th, typeID = tid)
-print trigger_lengths(th, configID = cid)
+th.insert( tr )
+
+ENSURE( len(find_triggers(th,tkey)) == 1, "there should be 1 trigger found" )
+
+ENSURE( n_triggers(th, sourceID = sid) == 1, "expected 1 source %s, found %d" \
+            % (str(sid), n_triggers(th,sourceID = sid) ) )
+ENSURE( n_triggers(th, typeID = tid) == 1, "expected 1 type %s found %d" \
+            % (str(tid), n_triggers(th,typeID = tid) ) )
+ENSURE( n_triggers(th, configID = cid) == 1, "expected 1 config %s found %d" \
+            % (str(cid), n_triggers(th,configID = cid) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid)) == 1, "expected 1 source %s, found %d" \
+            % (str(sid), len(trigger_lengths(th,sourceID = sid) ) ) )
+ENSURE( len(trigger_lengths(th, typeID = tid)) == 1, "expected 1 type %s found %d" \
+            % (str(tid), len(trigger_lengths(th,typeID = tid) ) ) )
+ENSURE( len(trigger_lengths(th, configID = cid)) == 1, "expected 1 config %s found %d" \
+            % (str(cid), len(trigger_lengths(th,configID = cid) ) ) )
+
+
 
