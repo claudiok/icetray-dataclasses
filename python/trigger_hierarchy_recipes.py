7a801c7d7bd69d9d3479e6f9f38d852203126313
Erik Blaufuss <blaufuss@icecube.umd.edu>
2012-03-29 12:54:01 +0000

    Bring forward fixes for trigger_hierarchy_recipes and test from r86672
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/releases/V12-03-00@86679 16731396-06f5-0310-8873-f7f720988828

--- python/trigger_hierarchy_recipes.py
+++ python/trigger_hierarchy_recipes.py
@@ -6,26 +6,30 @@ from icecube.dataclasses import TriggerKey
 def find_triggers(th, key):
     return_list = list()
     for t in th:
-        if t.trigger_key == key:
+        if t.key == key:
             return_list.append(t)
     return return_list
 
-def n_triggers( th, sourceID = None, typeID = None, configID = None):
-    if not ( sourceID or typeID or configID) : return 0
+def n_triggers(th, sourceID = None, typeID = None, configID = None):
+    if sourceID == None and \
+            typeID == None and \
+            configID == None : return 0
     return_value = 0
     for t in th:
-        if ( not sourceID or t.source_id == sourceID ) and \
-        ( not typeID or t.type_id == typeID ) and \
-        ( not configID or t.config_id == configID ) :
+        if ( sourceID == None or t.key.source == sourceID ) and \
+        ( typeID == None or t.key.type == typeID ) and \
+        ( configID == None or t.key.config_id == configID ) :
             return_value += 1
     return return_value      
 
-def trigger_lengths( th, sourceID = None, typeID = None, configID = None):
-    return_list = list()
-    if not ( sourceID or typeID or configID) : return return_list
+def trigger_lengths(th, sourceID = None, typeID = None, configID = None):
+    return_list = list() 
+    if sourceID == None and \
+            typeID == None and \
+            configID == None : return return_list
     for t in th:
-        if ( not sourceID or t.source_id == sourceID ) and \
-        ( not typeID or t.type_id == typeID ) and \
-        ( not configID or t.config_id == configID ) :
-            return_list.append( t.trigger_length )
+        if ( sourceID == None or t.key.source == sourceID ) and \
+        ( typeID == None or t.key.type == typeID ) and \
+        ( configID == None or t.key.config_id == configID ) :
+            return_list.append( t.length )
     return return_list
--- resources/test/test_trigger_hierachy.py
+++ resources/test/test_trigger_hierachy.py
@@ -7,21 +7,79 @@ from os.path import expandvars
 from I3Tray import I3Units
 from icecube.icetray.I3Test import *
 
+tkey_l = list()
+sid1 = dataclasses.I3Trigger.IN_ICE
+tid1 = dataclasses.I3Trigger.SIMPLE_MULTIPLICITY
+cid1 = 1008
+tkey1 = dataclasses.TriggerKey(sid1, tid1, cid1)
+tkey_l.append( tkey1 )
+
+tr1 = dataclasses.I3Trigger()
+tr1.key = tkey1
+
+sid2 = dataclasses.I3Trigger.IN_ICE
+tid2 = dataclasses.I3Trigger.SIMPLE_MULTIPLICITY
+cid2 = 1010
+tkey2 = dataclasses.TriggerKey(sid2, tid2, cid2)
+tkey_l.append( tkey2 )
+
+tr2 = dataclasses.I3Trigger()
+tr2.key = tkey2
+
 th = dataclasses.I3TriggerHierarchy()
+th.insert( tr1 )
+th.insert( tr2 )
+
+for tk in tkey_l :
+    ENSURE( len(find_triggers(th,tk)) == 1, "there should be 1 trigger found" )
+
+ENSURE( n_triggers(th, sourceID = sid1) == 2, "expected 2 source %s, found %d" \
+            % (str(sid1), n_triggers(th,sourceID = sid1) ) )
+
+ENSURE( n_triggers(th, typeID = tid1) == 2, "expected 2 type %s found %d" \
+            % (str(tid1), n_triggers(th,typeID = tid1) ) )
+
+ENSURE( n_triggers(th, sourceID = sid1, configID = cid1) == 1, "expected 1 config %s found %d" \
+            % (str(cid1), n_triggers(th,sourceID = sid1, configID = cid1) ) )
+
+ENSURE( n_triggers(th, typeID = tid1, configID = cid1) == 1, "expected 1 config %s found %d" \
+            % (str(cid1), n_triggers(th,sourceID = sid1, configID = cid1) ) )
+
+ENSURE( n_triggers(th, sourceID = sid1, typeID = tid1, configID = cid1) == 1, "expected 1 config %s found %d" \
+            % (str(cid1), n_triggers(th,sourceID = sid1, typeID = tid1, configID = cid1) ) )
+
+ENSURE( n_triggers(th, sourceID = sid2, configID = cid2) == 1, "expected 1 config %s found %d" \
+            % (str(cid2), n_triggers(th,sourceID = sid2, configID = cid2) ) )
+
+ENSURE( n_triggers(th, typeID = tid2, configID = cid2) == 1, "expected 1 config %s found %d" \
+            % (str(cid2), n_triggers(th,typeID = tid2, configID = cid2) ) )
+
+ENSURE( n_triggers(th, sourceID = sid2, typeID = tid2, configID = cid2) == 1, "expected 1 config %s found %d" \
+            % (str(cid2), n_triggers(th,sourceID = sid2, typeID = tid2, configID = cid2) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid1)) == 2, "expected 2 source %s, found %d" \
+            % (str(sid1), len(trigger_lengths(th,sourceID = sid1) ) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid1, typeID = tid1)) == 2, "expected 2 source %s, found %d" \
+            % (str(sid1), len(trigger_lengths(th,sourceID = sid1, typeID = tid1) ) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid1, typeID = tid1, configID = cid1 )) == 1, "expected 1 source %s, found %d" \
+            % (str(sid1), len(trigger_lengths(th,sourceID = sid1, typeID = tid1, configID = cid1 ) ) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid2)) == 2, "expected 2 source %s, found %d" \
+            % (str(sid2), len(trigger_lengths(th,sourceID = sid2) ) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid2, typeID = tid2)) == 2, "expected 2 source %s, found %d" \
+            % (str(sid2), len(trigger_lengths(th,sourceID = sid2, typeID = tid2) ) ) )
+
+ENSURE( len(trigger_lengths(th, sourceID = sid2, typeID = tid2, configID = cid2 )) == 1, "expected 1 source %s, found %d" \
+            % (str(sid2), len(trigger_lengths(th,sourceID = sid2, typeID = tid2, configID = cid2 ) ) ) )
+
+ENSURE( len(trigger_lengths(th, typeID = tid1)) == 2, "expected 2 type %s found %d" \
+            % (str(tid1), len(trigger_lengths(th,typeID = tid1) ) ) )
+
+ENSURE( len(trigger_lengths(th, configID = cid1)) == 1, "expected 2 config %s found %d" \
+            % (str(cid1), len(trigger_lengths(th,configID = cid1) ) ) )
 
 
-
-sid = dataclasses.I3Trigger.IN_ICE
-tid = dataclasses.I3Trigger.SIMPLE_MULTIPLICITY
-cid = 1008
-tkey = dataclasses.TriggerKey(sid, tid, cid)
-print find_triggers(th,tkey)
-
-print n_triggers(th, sourceID = sid)
-print n_triggers(th, typeID = tid)
-print n_triggers(th, configID = cid)
-
-print trigger_lengths(th, sourceID = sid)
-print trigger_lengths(th, typeID = tid)
-print trigger_lengths(th, configID = cid)
 
