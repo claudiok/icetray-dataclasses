claudiok/icetray-dataclasses

commit 22cae49a70f57b13f5e17f58bd6cd930bfc5d2b0

deprecating weight and replacing it with NPE and charge.
git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@86640 16731396-06f5-0310-8873-f7f720988828

private/dataclasses/physics/I3MCHit.cxx
@@ -10,21 +10,43 @@ void I3MCHit::SetParticleID(const I3Particle& p) {
 
 
 template <class Archive>
-  void I3MCHit::serialize (Archive &ar, unsigned version)
-  {
+void I3MCHit::save (Archive &ar, const unsigned version) const
+{
+  //always write the latest version
+  ar & make_nvp("time",time_);
+  ar & make_nvp("hitID",hitID_);
+  ar & make_nvp("ParticleID", particleID_);
+  ar & make_nvp("ParticleMajorID", particleMajorID_);
+  ar & make_nvp("CherenkovDistance", cherenkovDistance_);
+  ar & make_nvp("HitSource", source_);
+  ar & make_nvp("NPE", npe_);
+  ar & make_nvp("Charge", charge_);
+}
+
+template <class Archive>
+void I3MCHit::load (Archive &ar, const unsigned version)
+{
   if (version>i3mchit_version_)
     log_fatal("Attempting to read version %u from file but running version %u of I3MCHit class.",version,i3mchit_version_);
+  
+  ar & make_nvp("time",time_);
+  ar & make_nvp("hitID",hitID_);
+  if(version>2){
+    ar & make_nvp("NPE", npe_);
+    ar & make_nvp("Charge", charge_);    
+  }else{
+    double weight;
+    ar & make_nvp("Weight", weight);
+    npe_ = static_cast<unsigned>(weight);
+  }
+  ar & make_nvp("ParticleID", particleID_);
+  if(version>0)
+    ar & make_nvp("ParticleMajorID", particleMajorID_);
+  ar & make_nvp("CherenkovDistance", cherenkovDistance_);
+  if(version>1)
+    ar & make_nvp("HitSource", source_);
 
-    ar & make_nvp("time",time_);
-    ar & make_nvp("hitID",hitID_);
-    ar & make_nvp("Weight", weight_);
-    ar & make_nvp("ParticleID", particleID_);
-    if(version>0)
-      ar & make_nvp("ParticleMajorID", particleMajorID_);
-    ar & make_nvp("CherenkovDistance", cherenkovDistance_);
-    if(version>1)
-      ar & make_nvp("HitSource", source_);
-  }     
+}
 
   
 

private/pybindings/I3MCHit.cxx
@@ -32,8 +32,8 @@ void register_I3MCHit()
     scope mchit_scope = 
       class_<I3MCHit, boost::shared_ptr<I3MCHit> >("I3MCHit")
       .def(init<uint64_t,int>() )
-      #define PROPERTIES (Time)(HitID)(Weight)(CherenkovDistance)(HitSource)
-      #define RO_PROPERTIES (ParticleMajorID)(ParticleMinorID)
+      #define PROPERTIES (Time)(HitID)(Weight)(CherenkovDistance)(HitSource)(Charge)(NPE)
+      #define RO_PROPERTIES (ParticleMajorID)(ParticleMinorID)(Weight)
       BOOST_PP_SEQ_FOR_EACH(WRAP_PROP, I3MCHit, PROPERTIES)
       BOOST_PP_SEQ_FOR_EACH(WRAP_PROP_RO, I3MCHit, RO_PROPERTIES)
       #undef PROPERTIES

public/dataclasses/physics/I3MCHit.h
@@ -24,7 +24,7 @@
  * CherenkovDistance- direct path distance to the track that generated
  * this hit.
  */
-static const unsigned i3mchit_version_ = 2;
+static const unsigned i3mchit_version_ = 3;
 
 /**
  * List the names of enumeration members defined in this file
@@ -59,7 +59,8 @@ class I3MCHit
   I3MCHit() : 
     time_(NAN), 
     hitID_(-1),
-    weight_(NAN), 
+    npe_(1), 
+    charge_(NAN), 
     particleID_(-1), 
     particleMajorID_(0), 
     cherenkovDistance_(NAN),
@@ -68,7 +69,8 @@ class I3MCHit
  I3MCHit(uint64_t mid, int id) : 
     time_(NAN), 
     hitID_(-1),
-    weight_(NAN), 
+    npe_(1),
+    charge_(NAN), 
     particleID_(id), 
     particleMajorID_(mid), 
     cherenkovDistance_(NAN),
@@ -84,12 +86,27 @@ class I3MCHit
 
   void SetHitID(int hitID){hitID_ = hitID;}
 
-  double GetWeight() const { return weight_; }
+  void SetWeight(double w) { 
+    log_warn("I3MCHit::SetWeight is deprecated.  Please use I3MCHit::SetCharge or I3MCHit::SetNPE."
+	     "  Assuming this is NPE for the time being.");
+    npe_ = static_cast<uint64_t>(w);
+  }
 
-  void SetWeight(double weight) { weight_ = weight; }
+  double GetWeight() const { 
+    log_warn("I3MCHit::GetWeight is deprecated.  Please use I3MCHit::GetCharge or I3MCHit::GetNPE.");
+    return static_cast<uint64_t>(npe_); 
+  }
+
+  uint64_t GetNPE() const { return npe_; }
+
+  void SetNPE(uint64_t npe) { npe_ = npe; }
+
+  double GetCharge() const { return charge_; }
+
+  void SetCharge(double charge) { charge_ = charge; }
 
   int GetParticleID() const { 
-    log_warn("I3MCHit::GetParticleID is deprecated.  Please use I3MCHit::GetParticleMinorID");
+    log_warn("I3MCHit::GetParticleID is deprecated.  Please use I3MCHit::GetParticleMinorID.");
     return particleID_; 
   }
 
@@ -119,7 +136,8 @@ class I3MCHit
   bool operator==(const I3MCHit& rhs) {
     return time_ == rhs.time_
       && hitID_ == rhs.hitID_
-      && weight_ == rhs.weight_
+      && charge_ == rhs.charge_
+      && npe_ == rhs.npe_
       && particleID_ == rhs.particleID_
       && particleMajorID_ == rhs.particleMajorID_
       && cherenkovDistance_ == rhs.cherenkovDistance_
@@ -130,7 +148,8 @@ class I3MCHit
 
   double time_;
   int hitID_;
-  double  weight_;
+  uint64_t  npe_;
+  double  charge_;
   int    particleID_;
   uint64_t    particleMajorID_;
   double  cherenkovDistance_;
@@ -138,7 +157,10 @@ class I3MCHit
 
   friend class boost::serialization::access;
 
-  template <class Archive> void serialize(Archive & ar, unsigned version);
+  template <class Archive> void load(Archive & ar, const unsigned version);
+  template <class Archive> void save(Archive & ar, const unsigned version) const ;
+
+  BOOST_SERIALIZATION_SPLIT_MEMBER()
 
 };
 

commit 0beab44b2d1fe6281746724dc038bf3786847b68

I3MCHit serialization had been broken since r86640 (introduction of NPE/Charge fields and depreciation of Weight). boost::serialization depends on the order of elements
git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@86917 16731396-06f5-0310-8873-f7f720988828

private/dataclasses/physics/I3MCHit.cxx
@@ -15,12 +15,12 @@ void I3MCHit::save (Archive &ar, const unsigned version) const
   //always write the latest version
   ar & make_nvp("time",time_);
   ar & make_nvp("hitID",hitID_);
+  ar & make_nvp("NPE", npe_);
+  ar & make_nvp("Charge", charge_);
   ar & make_nvp("ParticleID", particleID_);
   ar & make_nvp("ParticleMajorID", particleMajorID_);
   ar & make_nvp("CherenkovDistance", cherenkovDistance_);
   ar & make_nvp("HitSource", source_);
-  ar & make_nvp("NPE", npe_);
-  ar & make_nvp("Charge", charge_);
 }
 
 template <class Archive>