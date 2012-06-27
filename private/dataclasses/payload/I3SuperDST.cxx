claudiok/icetray-dataclasses

commit 8a95f2e398aa317a6f3fcece60e70e7e76a03613

Use I3_SPLIT_SERIALIZABLE() for these classes with split serializations and clean up any preexisting hacks meant to do the same thing. This gets dataclasses (though not dataclasses pybindings) compiling with ICC 12.
git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@85667 16731396-06f5-0310-8873-f7f720988828

private/dataclasses/payload/I3SuperDST.cxx
@@ -756,6 +756,16 @@ namespace I3SuperDSTUtils {
 				size_ |= (size_t(bytes[i]) << (i*8));
 		}
 	}
+
+}
+
+I3_SPLIT_SERIALIZABLE(I3SuperDSTUtils::SizeCodec);
+
+template <class Archive>
+void
+I3SuperDST::save(Archive& ar, unsigned version) const
+{
+	save(ar, version, NULL);
 }
 
 template <class Archive>
@@ -1217,11 +1227,5 @@ I3SuperDST::load(Archive& ar, unsigned version)
 	return;
 }
 
-I3_SERIALIZABLE(I3SuperDST);
+I3_SPLIT_SERIALIZABLE(I3SuperDST);
 
-// explicitly instantiate serialization functions to avoid linker errors
-// when compiling superdst-test
-template void I3SuperDSTUtils::SizeCodec::save(boost::archive::portable_binary_oarchive&, unsigned) const;
-template void I3SuperDSTUtils::SizeCodec::load(boost::archive::portable_binary_iarchive&, unsigned);
-template void I3SuperDSTUtils::SizeCodec::load(boost::archive::xml_iarchive&, unsigned);
-template void I3SuperDSTUtils::SizeCodec::save(boost::archive::xml_oarchive&, unsigned) const;

private/dataclasses/I3MapOMKeyMask.cxx
@@ -622,4 +622,4 @@ I3RecoPulseSeriesMapMask::save(Archive & ar, unsigned version) const
 	ar & make_nvp("ElementMasks", elements);
 }
 
-I3_SERIALIZABLE(I3RecoPulseSeriesMapMask);
+I3_SPLIT_SERIALIZABLE(I3RecoPulseSeriesMapMask);

private/dataclasses/TriggerKey.cxx
@@ -198,4 +198,4 @@ std::ostream& operator<<(std::ostream& oss, const TriggerKey& k){
 }
 
   
-I3_SERIALIZABLE(TriggerKey);
+I3_SPLIT_SERIALIZABLE(TriggerKey);

private/dataclasses/payload/I3SuperDSTTrigger.cxx
@@ -182,4 +182,4 @@ I3SuperDSTTriggerSeries::load(Archive & ar, unsigned version)
 	}
 }
 
-I3_SERIALIZABLE(I3SuperDSTTriggerSeries);
+I3_SPLIT_SERIALIZABLE(I3SuperDSTTriggerSeries);

private/dataclasses/physics/I3AMANDAAnalogReadout.cxx
@@ -53,7 +53,7 @@ I3AMANDAAnalogReadout::load(Archive& ar, unsigned version)
   processTOTs_ = true;
 }
 
-I3_SERIALIZABLE(I3AMANDAAnalogReadout);
+I3_SPLIT_SERIALIZABLE(I3AMANDAAnalogReadout);
 I3_SERIALIZABLE(I3AMANDAAnalogReadoutMap);
 
 double I3AMANDAAnalogReadout::GetFirstLE() const

private/dataclasses/physics/I3DOMLaunch.cxx
@@ -281,6 +281,6 @@ std::ostream& operator<<(std::ostream& oss, const I3DOMLaunch& d)
 }
 
 
-I3_SERIALIZABLE(I3DOMLaunch);
+I3_SPLIT_SERIALIZABLE(I3DOMLaunch);
 
 I3_SERIALIZABLE(I3DOMLaunchSeriesMap);

private/dataclasses/physics/I3MCTree.cxx
@@ -1,4 +1,4 @@
 #include <icetray/serialization.h>
 #include "dataclasses/physics/I3MCTree.h"
 
-I3_SERIALIZABLE(I3MCTree);
+I3_SPLIT_SERIALIZABLE(I3MCTree);

private/dataclasses/physics/I3Particle.cxx
@@ -840,6 +840,6 @@ std::ostream& operator<<(std::ostream& oss, const I3Particle& p){
 }
 
 
-I3_SERIALIZABLE(I3Particle);
+I3_SPLIT_SERIALIZABLE(I3Particle);
 
 I3_SERIALIZABLE(I3ParticleVect);

commit b13b88a64770ee8d892d4ce5e53c7b90edce8756

Fix SuperDST serialization partially broken in r85667.
git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@90038 16731396-06f5-0310-8873-f7f720988828

private/dataclasses/payload/I3SuperDST.cxx
@@ -759,8 +759,6 @@ namespace I3SuperDSTUtils {
 
 }
 
-I3_SPLIT_SERIALIZABLE(I3SuperDSTUtils::SizeCodec);
-
 template <class Archive>
 void
 I3SuperDST::save(Archive& ar, unsigned version) const
@@ -1228,4 +1226,12 @@ I3SuperDST::load(Archive& ar, unsigned version)
 }
 
 I3_SPLIT_SERIALIZABLE(I3SuperDST);
+
+// explicitly instantiate serialization functions to avoid linker errors
+// when compiling superdst-test
+template void I3SuperDSTUtils::SizeCodec::save(boost::archive::portable_binary_oarchive&, unsigned) const;
+template void I3SuperDSTUtils::SizeCodec::load(boost::archive::portable_binary_iarchive&, unsigned);
+template void I3SuperDSTUtils::SizeCodec::load(boost::archive::xml_iarchive&, unsigned);
+template void I3SuperDSTUtils::SizeCodec::save(boost::archive::xml_oarchive&, unsigned) const;
+
 