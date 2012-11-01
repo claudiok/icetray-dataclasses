050b3f01396bfb541c57b7bb47b8243affe5c8e1
Claudio Kopper <ckopper@icecube.wisc.edu>
2012-11-01 21:21:27 +0000

    do not mix tabs and spaces for indenting
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@94873 16731396-06f5-0310-8873-f7f720988828

--- public/dataclasses/copy_if.h
+++ public/dataclasses/copy_if.h
@@ -15,18 +15,18 @@
 #define COPY_IF_H_INCLUDED
 
 template <typename InputIterator,
-	  typename OutputIterator,
-	  typename Predicate>
+          typename OutputIterator,
+          typename Predicate>
 OutputIterator 
 copy_if(InputIterator begin,
-	InputIterator end,
-	OutputIterator destBegin,
-	Predicate p)
+        InputIterator end,
+        OutputIterator destBegin,
+        Predicate p)
 {
   while (begin != end)
     {
       if (p(*begin))
-	*destBegin++ = *begin;
+        *destBegin++ = *begin;
       ++begin;
     }
   return destBegin;
