3a96a9b51988e8fec8a978b569672297794f81d4
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-02-10 22:02:47 +0000

    Hackaround for some stupid compiler bug that happens with clang 3.2 in at high optimizations. Hopefully they fix it eventually.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@98793 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -1,6 +1,15 @@
 from icecube.load_pybindings import load_pybindings
 import icecube.icetray # be nice and pull in our dependencies
 
+# XXX Work around a stupid bug with certain combinations of Boost, compiler, and
+# RTLD where pulling in libdataclasses as a dependency of the pybindings will
+# cause corruption of the internal state of the extended_type_info database
+# when classes in dataclasses are inherited from in certain circumstances I
+# can't identify.
+# Observed on: FreeBSD 10 + Boost 1.52 + clang 3.2 when loading mutineer
+# (of all things)
+icecube.icetray.load('dataclasses', False)
+
 load_pybindings(__name__,__path__)
 
 
