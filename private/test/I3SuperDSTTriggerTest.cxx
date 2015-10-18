40cd61b13bb6edde9530f595dbc26c9973906d54
David Schultz <dschultz@icecube.wisc.edu>
2015-10-18 20:45:33 +0000

    make buildbots greener, and add better coverage to I3SuperDSTTrigger
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@138649 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3SuperDSTTriggerTest.cxx
+++ private/test/I3SuperDSTTriggerTest.cxx
@@ -1,6 +1,7 @@
 #include <I3Test.h>
 #include <dataclasses/physics/I3Trigger.h>
 #include <dataclasses/status/I3DetectorStatus.h>
+#include <dataclasses/status/I3TriggerStatus.h>
 #include <dataclasses/payload/I3SuperDSTTrigger.h>
 
 
@@ -11,8 +12,25 @@ TEST(i3objects_constructor)
 {
   I3Trigger trigger = I3Trigger();
   I3DetectorStatus detectorstatus = I3DetectorStatus();
+  detectorstatus.triggerStatus[trigger.GetTriggerKey()] = I3TriggerStatus();
   I3SuperDSTTrigger(trigger,detectorstatus);
-  
+}
+
+TEST(i3objects_constructor_bad)
+{
+  I3Trigger trigger = I3Trigger();
+  I3DetectorStatus detectorstatus = I3DetectorStatus();
+  int caught = 0;
+  try
+  {
+    I3SuperDSTTrigger(trigger,detectorstatus);
+  }
+  catch (const std::range_error& e)
+  {
+    caught = 1;
+  }
+  if (caught != 1)
+    throw std::runtime_error("did not raise error like expected");
 }
 
 TEST(idx_constructor)
