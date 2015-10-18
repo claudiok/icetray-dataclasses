6aa8e55eece2484199401f28880f9296c800af2d
Tessa Carver <Tessa.Carver@unige.ch>
2015-10-18 13:31:13 +0000

    test Trigger Key
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@138637 16731396-06f5-0310-8873-f7f720988828

--- resources/test/test_TriggerKey.py
+++ resources/test/test_TriggerKey.py
@@ -7,8 +7,14 @@ from I3Tray import I3Units
 class Mytest(unittest.TestCase):
 
 	def test_example(self):
-		t = dataclasses.TriggerKey()
+		t = dataclasses.TriggerKey() 
 		print t
-		#self.assertEqual(False, True, "these should be equal")
+		t2 = dataclasses.TriggerKey(dataclasses.I3Trigger.EXTERNAL, dataclasses.I3Trigger.STRING)
+		print t2
+		t3 =  dataclasses.TriggerKey(dataclasses.I3Trigger.AMANDA_MUON_DAQ, dataclasses.I3Trigger.STRING)
+		print t3
+		t4 =  dataclasses.TriggerKey(dataclasses.I3Trigger.AMANDA_TWR_DAQ, dataclasses.I3Trigger.STRING)
+		print t4
+
 
 unittest.main()
