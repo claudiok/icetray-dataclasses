f83441e65ceaf842070e8ab0f60215b26fc7ae9f
Jakob van Santen <jvansanten@gmail.com>
2016-10-19 06:34:29 +0000

    Expose I3ParticleID's real hash to Python
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/dataclasses/trunk@150903 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/I3ParticleID.cxx
+++ private/pybindings/I3ParticleID.cxx
@@ -3,11 +3,20 @@
 
 using namespace boost::python;
 
+namespace {
+
+int hash_id(const I3ParticleID &id){
+	return hash<I3ParticleID>()(id);
+}
+
+}
+
 void register_I3ParticleID()
 {
 	class_<I3ParticleID, boost::shared_ptr<I3ParticleID> >("I3ParticleID")
 	.def_readwrite("majorID",&I3ParticleID::majorID)
 	.def_readwrite("minorID",&I3ParticleID::minorID)
 	.def(dataclass_suite<I3ParticleID>())
+	.def("__hash__", &hash_id)
 	;
 }
